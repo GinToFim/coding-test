15cbe4cac91c677dbaa3907f7132695e3c0a431d
GinToFim <shlf6673@gmail.com>
2022-10-04 18:48:23 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,17 +1,37 @@
+# 아이디어 : 
+# 1. 앞서 나온 단어를 기억하고 있어야 함 -> dict 저장
+# 2. 단어의 맨 앞과 맨 끝을 비교 (끝말잇기)
+# 3. n번 사람(num), n번 차례(times)
+
+# 알고리즘 : 최대 O(n^2) => 10000 
+# 자료구조 : 해시-맵 사용
+
 def solution(n, words):
     answer = []
-    turn = 0
-    wordList = [words[0]]
 
-    for idx in range(1, len(words)):
-        if words[idx-1][-1] != words[idx][0]:
-            turn = idx
-            break
-        if words[idx] in wordList:
-            turn = idx
-            break
-        wordList.append(words[idx])
-    answer = [turn%n +1, turn//n + 1]
-    if turn == 0:
-        answer = [0, 0]
-    return answer
+    # 모든 단어 False로 초기화
+    check = dict()
+    for word in words :
+        check[word] = False
+    
+    # 첫 번째 단어는 바로 실행
+    turn = 0
+    check[words[0]] = True
+    
+    for i in range(1, len(words)) :    
+        # 끝말잇기가 틀렸다면
+        if words[i][0] != words[i-1][-1] :
+            turn = i
+            return [turn%n + 1, turn//n + 1]
+        
+        # 이미 썻던 단어라면 
+        if check[words[i]] == True :
+            turn = i
+            return [turn%n + 1, turn//n + 1]
+        
+        # 통과한 단어 체크 표시
+        check[words[i]] = True
+        
+    # for문을 탈출했다면(주어진 단어로 끝말잇기가 끝났다면)
+    return [0, 0]
+    
