4a693ee95d6310540dd022c70cf52fcd874455b5
GinToFim <shlf6673@gmail.com>
2022-10-06 01:39:06 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.46 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,7 +1,7 @@
 # 아이디어 : 완전 탐색으로 2 x 2-4개가 모두 동일하다면 인덱스를 기억해놓고 없애기
 #           1. (x, y) - (x+1, y), (x, y+1), (x+1, y+1) 이 동일하다면 인덱스를 따로 기억
 #           2. 바로 없앤다면 어떤 경우에는 못 찾을수도
-#           3. 없앰 = 'X' 문자로 변경 (어떻게 올리지)
+#           3. 없앰 = 'X' 문자로 변경 
 #           4. flag로 없앨 수 있는 블록이 있는지 없는지 비교 
 #              만약 없다면 점수 return
 # 알고리즘 : 브루트 포스 - O(n^3) = 30^3 = 27000
@@ -19,40 +19,16 @@ def solution(m, n, board):
     
     while not check :
         check = True 
-        explode_idx = [] # 터지는 블록 기억
         
-        for x in range(m) :
-            for y in range(n) :
-                block = board[x][y]
-                if block == '0' :
-                    continue
-                try :
-                    if block == board[x+1][y] and block == board[x][y+1] and block == board[x+1][y+1] :
-                            explode_idx.append((x, y))
-                            check = False
-                except IndexError :
-                    continue
-
-        for ex, ey in explode_idx :
+        check, explode_list = getExplodeList(m, n, board)
+        
+        for ex, ey in explode_list :
             board[ex][ey] = '0'
             board[ex+1][ey] = '0'
             board[ex][ey+1] = '0'
             board[ex+1][ey+1] = '0'
 
-        for y in range(n) :
-            tmp_board = []
-            for x in range(m) :
-                # 0를 맨 앞으로 옮기기
-                if board[x][y] == '0' :
-                    tmp_board.append(board[x][y])
-
-            for x in range(m) :
-                if board[x][y] != '0' :
-                    tmp_board.append(board[x][y])
-
-            for x in range(m) :
-                board[x][y] = tmp_board[x]
-        
+        board = chagePlace(m, n, board)
         
     for x in range(m) :
         for y in range(n) :
@@ -60,3 +36,41 @@ def solution(m, n, board):
                 answer += 1
         
     return answer
+
+# get 터질 블록 리스트 함수
+def getExplodeList(m, n, board) :
+    check = True
+    explode_list = [] # 터지는 블록 기억
+
+    for x in range(m) :
+        for y in range(n) :
+            block = board[x][y]
+            if block == '0' :
+                continue
+            try :
+                if block == board[x+1][y] and block == board[x][y+1] and block == board[x+1][y+1] :
+                        explode_list.append((x, y))
+                        check = False
+            except IndexError :
+                continue
+    
+    return check, explode_list
+
+# 터진 자리를 올리는 함수
+def chagePlace(m, n, board) :
+    for y in range(n) :
+        tmp_board = []
+        for x in range(m) :
+            # 0를 맨 앞으로 옮기기
+            if board[x][y] == '0' :
+                tmp_board.append(board[x][y])
+
+        for x in range(m) :
+            if board[x][y] != '0' :
+                tmp_board.append(board[x][y])
+
+        for x in range(m) :
+            board[x][y] = tmp_board[x]
+    
+    return board
+    
