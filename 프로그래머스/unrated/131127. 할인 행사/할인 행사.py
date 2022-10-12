1e4867e971028572ae32ba3afd2d2a67aeb408e7
GinToFim <shlf6673@gmail.com>
2022-10-12 11:50:13 +0900

    [unrated] Title: 할인 행사, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/unrated/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/unrated/131127. 할인 행사/할인 행사.py
@@ -1,12 +1,17 @@
-def solution(want, number, discount):
+def solution(want, number, discount) -> int:
+    today_discount = {key: discount[:10].count(key) for key in set(discount[:10])}
     answer = 0
-    while len(discount) >= 10:
-        days = discount[:10]
-        for item, n in zip(want, number):
-            if days.count(item) !=n:
+    for i in range(len(discount) - 10 + 1):
+        for key, value in zip(want, number):
+            if today_discount.get(key, 0) < value:
                 break
-            if item == want[-1]:
-                answer += 1
-        discount.pop(0)
-        
+        else:
+            answer += 1
+        # sliding_window기법을 사용했지만, 사실 이번 문제에서는 안써도 비슷하다.
+        # 이유: 리스트의 합이나 곱같은 문제가 아니고 리스트를 슬라이싱만 해도 해결이됨.
+        if i != len(discount) - 10:
+            today_discount[discount[i]] -= 1
+            today_discount[discount[i + 10]] = (
+                today_discount.get(discount[i + 10], 0) + 1
+            )
     return answer
