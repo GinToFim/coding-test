9e9f653402e0dc33231b85a33eba1813f97945f1
GinToFim <shlf6673@gmail.com>
2022-10-12 10:27:24 +0900

    [unrated] Title: 할인 행사, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/unrated/131127. 할인 행사/할인 행사.py
@@ -1,27 +1,12 @@
-# 아이디어
-# 알고리즘 
-# 자료구조 : 해시-맵 사용, dict 사용
-
 def solution(want, number, discount):
-    discount_dict = dict()
-    
-    for i in range(len(discount)-10+1) :
-        sort_discount = sorted(discount[i:i+10])
-        key = ''.join(sort_discount)
+    answer = 0
+    while len(discount) >= 10:
+        days = discount[:10]
+        for item, n in zip(want, number):
+            if days.count(item) !=n:
+                break
+            if item == want[-1]:
+                answer += 1
+        discount.pop(0)
         
-        if key not in discount_dict :
-            discount_dict[key] = 1
-        else :
-            discount_dict[key] += 1
-    
-    want_list = []
-    for i in range(len(want)) :
-        for _ in range(number[i]) :
-            want_list.append(want[i])
-
-    want_key = ''.join(sorted(want_list))
-    
-    if want_key in discount_dict.keys() :
-        return discount_dict[want_key]
-    else :
-        return 0
+    return answer
