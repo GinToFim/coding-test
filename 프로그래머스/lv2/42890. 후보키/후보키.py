6ef2cada721766cbdd754ed5b6b34a7d82d56610
GinToFim <shlf6673@gmail.com>
2022-10-15 13:58:15 +0900

    [level 2] Title: 후보키, Time: 0.39 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.py
+++ 프로그래머스/lv2/42890. 후보키/후보키.py
@@ -1,27 +1,30 @@
 from itertools import combinations
 
 def solution(relation):
+    answer = 0
+    
     row = len(relation)
     col = len(relation[0])
 
     #가능한 속성의 모든 인덱스 조합 
-    combi = []
+    combi_list = []
     for i in range(1, col+1):
-        combi.extend(combinations(range(col), i))
-        
-    #유일성
+        combi_list.extend(combinations(range(col), i))
+    
+    # 유일성
     unique = []
-    for i in combi:
-        tmp = [tuple([item[key] for key in i]) for item in relation]
-
-        if len(set(tmp)) == row:    # 유일성
-            put = True
+    for combi in combi_list : 
+        tmp = [tuple([item[x] for x in combi]) for item in relation]
+        if len(set(tmp)) == row : # 유일성 체크
+            check = True
             
-            for x in unique:
-                if set(x).issubset(set(i)):  # 최소성
-                    put = False
+            # unique가 비어있으면 자동으로 통과
+            for x in unique : 
+                if set(x).issubset(set(combi)) : # 최소성 체크
+                    check = False
                     break
-                    
-            if put: unique.append(i)
             
+            if check :
+                unique.append(combi)
+        
     return len(unique)
