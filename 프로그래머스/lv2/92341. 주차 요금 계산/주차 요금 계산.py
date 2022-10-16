397c0e3da9a16c72cefdb8885fbe0a546cdd7a93
GinToFim <shlf6673@gmail.com>
2022-10-16 16:17:34 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,50 +1,28 @@
 import math
+from collections import defaultdict
 
-def solution(fees, records):
-    answer = []
-    
-    # 기본(단위) 시간(요금)을 분배
-    default_time = fees[0]
-    default_price = fees[1]
-    per_time = fees[2]
-    per_price = fees[3]
-    
-    # True False로 구분
-    parking_car = dict() # 현재 주차된 상태의 차를 dictionary에 저장
-    park_time = dict() # 주차된 차의 시각을 저장 
-    total_time = dict() # 주차된 차량의 총 시간을 저장 
+def get_fee(minutes, fees) :
+    # default, per
+    dm, dp, pm, pp = fees
+    return dp + math.ceil(max(0, minutes-dm) / pm) * pp
+
+def solution(fees, records): 
+    parking = {} # 현재 주차중인 차를 기록하는 딕셔너리
+    park_time = defaultdict(int) # 차가 주차된 시간을 기록하는 딕셔너리
     
     for record in records :
-        time, num, how = record.split()
-        time = int(time[:2]) * 60 + int(time[-2:])
+        _time, car, cmd = record.split()
+        hour, minute = _time.split(':')
+        minutes = int(hour) * 60 + int(minute)
         
-        if how == 'IN' :
-            parking_car[num] = True
-            park_time[num] = time
+        if cmd == 'IN' :
+            parking[car] = minutes
         else :
-            parking_car[num] = False
-            if num not in total_time :
-                total_time[num] = time - park_time[num]
-            else :
-                total_time[num] += time - park_time[num]
-                
+            park_time[car] += minutes - parking[car]
+            del parking[car]
     
-    for num in parking_car.keys() :
-        if parking_car[num] :
-            parking_car[num] = False
-            if num not in total_time :
-                total_time[num] = 23*60 + 59 - park_time[num]
-            else :
-                total_time[num] += 23*60 + 59 - park_time[num]
+    # 남아있는 차를 23:59으로 빼기
+    for car in parking.keys() :
+        park_time[car] += 23*60 + 59 - parking[car]
     
-    result = dict()
-    
-    for num in sorted(total_time.keys()) :
-        time = total_time[num]
-        total_price = default_price
-        plus_time = time - default_time
-        if plus_time > 0 :
-            total_price += math.ceil(plus_time/per_time) * per_price
-        answer.append(total_price)
-    
-    return answer
+    return [get_fee(park_time[car], fees) for car in sorted(park_time.keys())]
