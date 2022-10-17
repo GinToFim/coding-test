b2a535cd69ef5e637dc7ca47c3d154cee3d35b40
GinToFim <shlf6673@gmail.com>
2022-10-18 00:49:21 +0900

    [level 2] Title: [3차] n진수 게임, Time: 10.86 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,25 +1,20 @@
-# 아이디어 : 1. 10진법 to n진법 함수 정의
-#           2. 모든 사람들이 말하는 개수 만큼 숫자 나열
-#           3. 튜브가 말하는 순서에 해당하는 문자 담기
-# 알고리즘 : 구현
-
 def solution(n, t, m, p):
-    num = t * m # 총 숫자의 개수
-    string = '' # 숫자를 나열할 문자열
-    result = '' # 튜브가 말해야하는 숫자(문자열)
+    num = t * m
+    string = ''
+    result = ''
+    string = ''
     
-    for i in range(num) :
-        string += convert_notation(i, n)
-    
-    for i in range(num) :
-        if (i+1) % m == p % m :
-            result += string[i]
+    for i in range(num):
+        string += num_to_base(i, n) # i(번째)를 n진법으로 변환
+        
+    for i in range(p-1, num, m):
+        # print(p-1, i, string[i])
+        result += string[i]
     
     return result
 
-# n진법 변환 함수
-def convert_notation(n, base) :
-    T = '0123456789ABCDEF'    
+def num_to_base(n, base) :
+    T = '0123456789ABCDEF'
     q, r = divmod(n, base)
     
-    return convert_notation(q, base) + T[r] if q > 0 else T[r]
+    return num_to_base(q, base) + T[r] if q > 0 else T[r]
