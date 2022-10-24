98fd8183888c243e008f5d5c988a75f9416bb53b
GinToFim <shlf6673@gmail.com>
2022-10-25 01:04:58 +0900

    [unrated] Title: 롤케이크 자르기, Time: 558.53 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,14 +1,12 @@
-# 아이디어 : topping 리스트의 길이만큼 for문을 돌면서
-#           set의 원소 개수가 같은 경우가 있는지 check
-# 알고리즘 : 브루트 포스, O(n^2) -> 실패
-#           
-# 자료구조 : set 
+# 아이디어 : 매번 set를 초기화하지말고
+#           add, remove로 원소를 추가/제거하기
+# 알고리즘
+# 자료구조
 
 from collections import Counter
 
 def solution(topping):
     answer = 0
-    
     set1 = set(topping)
     set2 = set()
     counter = Counter(topping)
@@ -17,10 +15,9 @@ def solution(topping):
         counter[topping[i]] -= 1
         if counter[topping[i]] == 0 :
             set1.remove(topping[i])
-    
-        set2.add(topping[i])
         
+        set2.add(topping[i])
         if len(set1) == len(set2) :
             answer += 1
-            
+    
     return answer
