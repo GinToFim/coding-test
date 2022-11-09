c2eccfd4b4ce9bb6e563a411e0e30390f86d9fe5
GinToFim <shlf6673@gmail.com>
2022-11-09 09:37:23 +0900

    [unrated] Title: 야간 전술보행, Time: 0.57 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133501. 야간 전술보행/야간 전술보행.py
+++ 프로그래머스/unrated/133501. 야간 전술보행/야간 전술보행.py
@@ -1,16 +1,22 @@
-# 아이디어 : 1. scope를 정렬하고, times_dict 만들기
-# 알고리즘
-# 자료구조
+# 아이디어 : 1. scope의 원소를 정렬하고 순번 append + scope 정렬
+#           2. 순번 : time으로 key-value하는 times_dict 생성
+#           3. time % (근무 + 휴식) <= 근무 으로 하면 마지막 근무시간에 안걸리므로
+#              (time-1) % (근무 + 휴식) <= 근무-1 로 수정
+# 알고리즘 : 정렬 + 구현? 그리디?
+
 
 def solution(distance, scope, times):
     n = len(scope)
+    # scope의 원소들을 정렬한 뒤 순번 append
     for i in range(n) :
         scope[i].sort()
         scope[i].append(i)
-    scope.sort()
+    scope.sort() # scope 정렬
     
+    # 순번 : time으로 하는 딕셔너리 생성
     times_dict = {i : times[i] for i in range(n)}
     
+    # 해당 경비병 근무 시간에 걸리면 바로 return 
     for i in range(n) :
         start, end, key = scope[i]
         work, rest = times_dict[key]
@@ -18,5 +24,6 @@ def solution(distance, scope, times):
         for num in range(start, end + 1) :
             if (num-1) % (work + rest) <= work-1 :
                 return num
-        
+    
+    # 안 걸리면 최대 거리 return
     return distance
