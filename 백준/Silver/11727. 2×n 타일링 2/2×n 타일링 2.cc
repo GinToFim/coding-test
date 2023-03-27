0084e9111c086d6b81c8ac3fc80c3f44034f7111
GinToFim <shlf6673@gmail.com>
2023-03-27 16:15:40 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1001
+
+using namespace std;
+
+int dp[MAX] = {0, };
+int n;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (int i = 3; i < n + 1; i++)
+        dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
+
+    cout << dp[n] << '\n'; 
+    return 0;
+}
