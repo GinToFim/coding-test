629c0a482bddc901e071539fd8723650bb12b42b
GinToFim <shlf6673@gmail.com>
2023-03-28 19:50:41 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    cout << n << '\n';
+    cout << 1 << '\n';
+    return 0;
+}
