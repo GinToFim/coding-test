b63d945abc0e73ebb2214443710190488f0618a6
GinToFim <shlf6673@gmail.com>
2023-03-29 19:50:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35264 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,28 +1,28 @@
+# 알고리즘 : bfs
+# 자료구조 : deque
+
 from collections import deque
 
 n, k = map(int, input().split())
-graph = [-1] * (100001)
+board = [-1] * 100001
 
-def bfs(start) :
-	# 시작노드 초기화
-	graph[start] = 0
-	queue = deque()
-	queue.append(start)
-
-	while queue :
-		x = queue.popleft()
-		nx_list = []
-		nx_list.append(x-1)
-		nx_list.append(x+1)
-		nx_list.append(x*2)
-
-		for nx in nx_list :
-			if nx <= -1 or nx >= 100001 :
-				continue
-
-			if graph[nx] == -1 :
-				graph[nx] = graph[x] + 1
-				queue.append(nx)
-				
-bfs(n)
-print(graph[k])
+def bfs(x) :
+    # 큐 및 시작노드 정의
+    queue = deque()
+    queue.append(x)
+    board[x] = 0
+    
+    # 큐가 빌 때까지
+    while queue :
+        v = queue.popleft()
+        
+        if v == k :
+            return board[v]
+        
+        for nv in (v - 1, v + 1, 2 * v) :
+            # 범위 이내이고 방문한 적이 없다면
+            if 0 <= nv <= 100000 and board[nv] == -1 :
+                board[nv] = board[v] + 1
+                queue.append(nv)
+                
+print(bfs(n))
