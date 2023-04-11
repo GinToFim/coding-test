58f7cb0ea9167e9d56969d6442d5478e172ffd69
GinToFim <shlf6673@gmail.com>
2023-04-11 18:01:12 +0900

    [Gold IV] Title: 거짓말, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,20 +1,16 @@
-# 아이디어 : 1. 파티에 대한 정보를 모두 입력
-#           2. 모든 파티에 대하여 union find 실행
-#           3. 그 후에 다시 모든 파티원들에 대하여 진실이 아는 사람이 없으면 카운트
-# 알고리즘 : union-find
-# 자료구조 : disjoint-set
-
 import sys
 input = sys.stdin.readline
 
-def find_parent(parent, x) :
+# find 연산 정의(경로 압축)
+def find(x) :
     if parent[x] != x :
-        parent[x] = find_parent(parent, parent[x])
+        parent[x] = find(parent[x])
     return parent[x]
 
-def union_parent(parent, a, b) :
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+# union 연산 정의
+def union(a, b) :
+    a = find(a)
+    b = find(b) 
     
     if a < b :
         parent[b] = a
@@ -23,45 +19,46 @@ def union_parent(parent, a, b) :
 
 n, m = map(int, input().split())
 
+# 진실을 아는 사람
 truths = list(map(int, input().split()))
-tcnt = truths[0]  # 진실을 아는 사람 수
-truths = truths[1:] # 진실을 아는 사람들
+truths = truths[1:]
 
+# 0번을 진실을 아는 사람으로 설정(why? 0이 제일 작음)
+KNOW_TRUTH = 0
 # 부모 테이블 정의
 parent = [v for v in range(n + 1)]
 
-# 파티 수
+for t in truths :
+    parent[t] = KNOW_TRUTH
+    
 parties = []
 for _ in range(m) :
-    parties.append(list(map(int, input().split())))
+    party_info = list(map(int, input().split()))
     
-for party_info in parties :
     p_cnt = party_info[0]
     party = party_info[1:]
     
+    # 파티의 참석한 사람들에 대해 2명씩 union 연산 수행
     for i in range(p_cnt - 1) :
-        union_parent(parent, party[i], party[i+1])
+        union(party[i], party[i+1])
         
-# 진실을 아는사람들의 부모 찾기
-truth_parent = [find_parent(parent, t) for t in truths]
-
+    parties.append(party_info)
+    
 result = 0
 
 for party_info in parties :
+    know = False
+    
     p_cnt = party_info[0]
     party = party_info[1:]
     
-    # 현재 파티원들에 부모 찾기
-    party_parent = [find_parent(parent, p) for p in party]    
-    
-    check = True
-    # 만약에 진실을 아는사람들의 부모와 파티원들에 부모가 겹치면 무시
-    for p in party_parent :
-        if p in truth_parent :
-            check = False
+    for i in range(p_cnt) :
+        # 진실을 알고 있는 그룹에 속했을 경우
+        if find(party[i]) == KNOW_TRUTH :
+            know = True
             break
     
-    if check :
+    if not know :
         result += 1
-    
+
 print(result)
