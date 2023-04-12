e0b721aaf872baf38012b7ed3d9579d18c7cffdd
GinToFim <shlf6673@gmail.com>
2023-04-12 18:31:15 +0900

    [Gold IV] Title: 네트워크 연결, Time: 240 ms, Memory: 46764 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,5 +1,5 @@
-# 알고리즘 : MST, kruskal(parent, union, find, edges, sort, cycle)
-# 자료구조 : union-find
+# 알고리즘 : MST, kruskal(parent, find, union, edges, sort, cycle)
+# 자료구조 : union, find
 
 import sys
 input = sys.stdin.readline
@@ -19,36 +19,36 @@ def union(a, b) :
         parent[b] = a
     else :
         parent[a] = b
-
+        
 n = int(input())
 m = int(input())
 
 # 부모 테이블 정의
 parent = [v for v in range(n + 1)]
 
-# 간선 테이블 정의
+# 간선 테이블
 edges = []
 
 for _ in range(m) :
     a, b, c = map(int, input().split())
-    edges.append((c, a, b)) # (비용, a, b) 순으로 저장
+    edges.append((c, a, b)) # (비용, a, b)순으로 저장
     
 # 간선 오름차순 정렬
 edges.sort()
 
-result = 0 # 총 비용
+result = 0 # 최소 총 비용
 cnt = 0 # 간선 개수
 
 for edge in edges :
     cost, a, b = edge
     
-    if cnt >= n - 1 :
+    if cnt == n - 1 :
         break
     
-    # 사이클이 발생하지 않았다면
+    # 사이클 발생 X
     if find(a) != find(b) :
         union(a, b)
         result += cost
         cnt += 1
-
+        
 print(result)
