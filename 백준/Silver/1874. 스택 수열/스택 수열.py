0554a9ab9753bf36718ab3912de2fe827838fd9b
GinToFim <shlf6673@gmail.com>
2023-04-13 20:00:59 +0900

    [Silver II] Title: 스택 수열, Time: 204 ms, Memory: 32840 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,36 +1,32 @@
 # 아이디어 : 
 # 알고리즘
-# 자료구조 : stack
+# 자료구조 : 스택
 
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-stack = []
-operators = []
+stack, operators = [], []
 check = True
+now = 1
 
-now = 1 # 현재 숫자
 for _ in range(n) :
     num = int(input())
-    
     while now <= num :
         stack.append(now)
-        operators.append("+")
+        operators.append('+')
         now += 1
     
-    # stack의 top과 주어진 숫자가 같으면 pop 연산
+    # 스택의 탑과 일치한다면
     if stack[-1] == num :
         stack.pop()
-        operators.append("-")
-    # 같지 않으면
+        operators.append('-')
     else :
         check = False
         break
-             
-if not check :
-    print("NO")
-else :
+    
+if check :
     for op in operators :
         print(op)
+else :
+    print('NO')
