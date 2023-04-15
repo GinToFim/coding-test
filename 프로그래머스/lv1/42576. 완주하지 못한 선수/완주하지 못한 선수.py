d71d1825c78d63db415a94a284a78ef21b403de8
GinToFim <shlf6673@gmail.com>
2023-04-15 16:42:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.62 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,16 @@
-from collections import Counter
+# SUM hash
+# hash 함수를 이용하여 sum_hash로 participant를 모두 더하고
+# completion를 모두 빼고 남은 값이 정답
 
 def solution(participant, completion):
-    p_cnt = Counter(participant)
-    c_cnt = Counter(completion)
-    fail = (p_cnt - c_cnt).most_common()[0][0]
+    sum_hash = 0
+    hash_dict = dict()
     
-    return fail
+    for part in participant :
+        hash_dict[hash(part)] = part
+        sum_hash += hash(part)
+        
+    for com in completion :
+        sum_hash -= hash(com)
+    
+    return hash_dict[sum_hash]
