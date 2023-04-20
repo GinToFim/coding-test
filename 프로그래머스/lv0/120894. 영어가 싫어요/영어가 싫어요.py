b6ec534bbdcd4d7ad04fe7ba63f884e3d700ebd2
GinToFim <shlf6673@gmail.com>
2023-04-20 21:37:25 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = ""
+    
+    num_dict = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", 
+                "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    
+    string = ''
+    for ch in numbers :
+        string += ch
+        
+        if string in num_dict :
+            answer += num_dict[string]
+            string = ''
+    
+    return int(answer)
