0dc74f3b0f01d1c903a0a2dc9f828f152c245215
GinToFim <shlf6673@gmail.com>
2023-04-21 00:00:08 +0900

    [unrated] Title: 숫자 변환하기, Time: 127.67 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,41 +1,21 @@
-# 아이디어 : 1. visited 테이블 만들기 (-1이 방문 X)
-#          2. (n 더하기) or (2 곱하기) or (3 곱하기) 를 한꺼번에 탐색
-#          3. 가장 빨리 y에 도착한 횟수를 return
-# 알고리즘 : bfs(visited, n, *2, *3)
-# 자료구조 : queue(deque)
-
-from collections import deque
+# 아이디어 : 1. dp 테이블 무한대로 초기화
+#           2. (x + n, 2 * x, 3 * x) min 값 구하기
+# 알고리즘 : Dynamic Programming
 
 def solution(x, y, n):
-    answer = 0
-    visited = [-1 for _ in range(y + 1)]
+    INF = 10 ** 7
+    dp = [INF for _ in range(y + 1)]
+    dp[x] = 0
     
-    def bfs(x) :
-        # 큐 및 시작노드 정의
-        queue = deque()
-        queue.append(x)
-        visited[x] = 0
-        
-        # 큐가 빌 때까지
-        while queue :
-            v = queue.popleft()
+    for i in range(x, y + 1) :
+        if dp[i] == INF :
+            continue
             
-            # 목적지에 도착했다면
-            if v == y :
-                return visited[v]
-        
-            for nv in (v + n, 2 * v, 3 * v) :
-                
-                # 범위 밖이라면 무시
-                if nv > y :
-                    continue
-
-                # 방문한 적이 없다면 횟수 추가
-                if visited[nv] == -1 :
-                    visited[nv] = visited[v] + 1
-                    queue.append(nv)
-        
-        # 도착할 수 없다면
+        for next_i in (i + n, 2 * i, 3 * i) :
+            if next_i <= y :
+                dp[next_i] = min(dp[next_i], dp[i] + 1)
+    
+    if dp[y] == INF :
         return -1
-        
-    return bfs(x)
+    else :
+        return dp[y]
