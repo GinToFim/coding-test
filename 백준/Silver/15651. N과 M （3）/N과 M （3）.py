e1e6e0417dbefc605b1fec2a2619b9fb0206a17e
GinToFim <shlf6673@gmail.com>
2023-04-23 16:56:28 +0900

    [Silver III] Title: N과 M (3), Time: 2012 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,6 @@
+from itertools import product 
+
 n, m = map(int, input().split())
 
-result = []
-
-def dfs(num) :
-    if num == m :
-        print(*result)
-        return
-    
-    for i in range(1, n + 1) :
-        result.append(i)
-        dfs(num + 1)
-        result.pop()
-
-dfs(0)
+for iters in product(range(1, n + 1), repeat=m) :
+    print(*iters)
