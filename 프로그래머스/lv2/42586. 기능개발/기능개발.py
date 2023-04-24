0a03fbcf885ab1290fc49599a71cf700fb638a22
GinToFim <shlf6673@gmail.com>
2023-04-24 19:20:09 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,32 +1,31 @@
-# day를 사용하기
+# 아이디어 : 1. p[0] + s[0] * days >= 100 이면 pop하기
+#           2. pop에 대한 개수만큼 완성인 상태 늘리기
+# 알고리즘 : 
 # 자료구조 : queue(deque)
 
 from collections import deque
 
 def solution(progresses, speeds):
     answer = []
+    days = 1 # 날짜
+    cnt = 0 # 배포 개수
+    
     progresses = deque(progresses)
     speeds = deque(speeds)
-    cnt = 0 # 배포할 개수
-    day = 0 # 지나는 날짜
     
-    # 큐가 빌 때까지
+    # progresses가 빌 때까지
     while progresses :
-        # 이번 날짜에 배포가 가능하다면
-        if (progresses[0] + day * speeds[0]) >= 100 :
+        if progresses[0] + speeds[0] * days >= 100 :
             progresses.popleft()
             speeds.popleft()
             cnt += 1
         else :
-            # 배포할 개수가 있다면
-            if cnt >= 1 :
+            if cnt > 0 :
                 answer.append(cnt)
                 cnt = 0
-            # 배포할 것도 없고 지금 날짜에는 아무것도 안되면
-            else :
-                day += 1
+            days += 1
     
-    # 마지막 배포할 개수 추가
+    # 남은 배포 append
     answer.append(cnt)
     
     return answer
