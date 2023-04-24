1d98beb77cbc10379a1344aa8710a3a2d4ab186c
GinToFim <shlf6673@gmail.com>
2023-04-24 19:51:51 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,33 +1,40 @@
-# second, bridge : 큐 사용
-# bridge의 합과 truck_weights[0] 이 제한 무게보다 작으면
-# second와 bridge 원소 추가
+# 아이디어 : 
+# 알고리즘 
+# 자료구조 : queue(deque)
 
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    result = 0 # 걸리는 시간
-    bridge = deque() # 다리 위 트럭들 queue
-    second = deque() # 초를 재는 queue
+    answer = 0 # 총 시간
     
-    bridge.append(truck_weights.pop(0))
-    second.append(0)
+    truck_weights = deque(truck_weights) # 다리를 건널 트럭들
+    bridge_second = deque() # 다리를 건너고 있는 시간들
+    bridge_weights = deque() # 다리 위에 무게
     
-    # bridge 가 빌 때까지
-    while bridge :
-        result += 1
+    # 맨 앞 트럭 한 대 추가하기
+    bridge_second.append(0)
+    bridge_weights.append(truck_weights.popleft())
+    
+    # 다리가 빌 때까지
+    while bridge_weights :
+        answer += 1
         
-        for i in range(len(second)) :
-            second[i] += 1
-            
-        if second[0] == bridge_length : 
-            bridge.popleft()
-            second.popleft()
-
+        # 1초씩 추가
+        bridge_second = deque(sec + 1 for sec in bridge_second)
+        
+        # 트럭이 도착했다면
+        if bridge_second[0] >= bridge_length :
+            bridge_second.popleft()
+            bridge_weights.popleft()
+        
+        # 남아있는 트럭이 있으면
         if len(truck_weights) > 0 :
-            if sum(bridge) + truck_weights[0] <= weight :
-                truck = truck_weights.pop(0)
-                bridge.append(truck)
-                second.append(0)
-                
-    result += 1
-    return result
+            # 그리고 아직 다리의 무게가 여유가 있으면
+            if sum(bridge_weights) + truck_weights[0] <= weight :
+                bridge_second.append(0)
+                bridge_weights.append(truck_weights.popleft())
+    
+    # 끝나는 시간 1초 추가하기
+    answer += 1
+    
+    return answer
