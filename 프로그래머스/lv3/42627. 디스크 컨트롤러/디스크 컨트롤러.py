dbf0008b1fa9832c905d484d9ac0d592585c3a79
GinToFim <shlf6673@gmail.com>
2023-04-25 20:08:03 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,31 +1,31 @@
-# 아이디어 : 
-# 알고리즘 : heap
-# 자료구조 : heapq(heap)
+# 아이디어 : 1. (소요시간, 요청시점)을 기준으로 heap 수행
+#           2. 
+# 알고리즘 : heap 
+# 자료구조 : heapq
 
 import heapq
 
 def solution(jobs):
+    answer = 0
     # 작업 요청이 가능한 시간(start), 현재 시간(now)
     start, now = -1, 0
-    cnt = 0 # 작업을 처리한 개수 
-    result = 0 # 총 시간
-    hq = [] # 힙큐
+    cnt = 0 # 작업이 처리된 개수
+    hq = []
     
     while cnt < len(jobs) :
-        for job in jobs : # job[0] : 시점, job[1] : 걸리는 시간
-            # 작업이 요청이 가능한 시간(start)과 현재 시간(now) 사이이면
+        for job in jobs :
+            # 작업 실행이 가능하다면
             if start < job[0] <= now :
-                heapq.heappush(hq, (job[1], job[0]))
-                
-        # 만약 heap에 원소가 있다면
-        if hq : 
-            njob = heapq.heappop(hq) # [걸리는 시간, 시점]
-            start = now # 작업 요청 가능한 시간을 현재 시간으로 옮김
-            now += njob[0] # 현재시간은 걸리는 시간 추가
-            result += now - njob[1] # 총 시간에는 현재시간 - 작업이 들어온 시점
-            cnt += 1 # 작업 개수 추가
-        # heap이 비워져있다면
-        else  :
-            now += 1 # 현재 시간 추가
+                heapq.heappush(hq, (job[1], job[0])) # 소요시간, 요청시간순으로
         
-    return result // len(jobs)
+        # 만약 hq에 원소가 있다면
+        if hq :
+            njob = heapq.heappop(hq)
+            start = now # 요청 가능한 시간을 현재 시간으로 변경
+            now += njob[0] # 소요시간 추가 
+            answer += now - njob[1] # 총 시간에는 (현재시간 - 요청시간) 추가
+            cnt += 1
+        else :
+            now += 1 # 현재시간 추가
+    
+    return answer // cnt
