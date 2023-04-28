9ad6d666aebeb62d41ad603f7cba0a2b694cc038
GinToFim <shlf6673@gmail.com>
2023-04-28 18:10:39 +0900

    [Gold IV] Title: 공유기 설치, Time: 468 ms, Memory: 40548 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,40 +1,36 @@
-# 아이디어 : 해당 거리가 가장 인접한 두 공유기 사이의 최대 거리인가?
-#           -> 해당 거리로 공유기 설치가 충분한가?
-# 알고리즘 : 이분 탐색(O(logn)), 파라메트릭 서치
+# 아이디어 : 이 정도 사이의 거리라면 공유기 설치가 가능한가?
+# 알고리즘 : 이분 탐색, 파라메트릭 서치
 
 import sys
 input = sys.stdin.readline
 
 n, c = map(int, input().split())
-data = []
-for _ in range(n) :
-    data.append(int(input()))
-    
-# 이분 탐색을 위해 오름차순 정렬
+data = [int(input()) for _ in range(n)]
+
+# 공유기 오름차순 정리
 data.sort()
 
-# end : 최대 - 최소
-start, end = 1, data[-1] - data[0]
-
+start = 1
+end = data[-1] - data[0]
 result = 0
 
 while start <= end :
     mid = (start + end) // 2
+    total = 1 # 공유기 설치 개수 (첫 번째 포함)
+    now = data[0]
     
-    cnt = 1 # 설치 공유기 대수
-    past = data[0]
-    
-    for i in range(1, n) :
-        # 해당 위치가 전에 (공유기 위치 + 공백) 보다 충분하다면
-        if data[i] >= past + mid :
-            past = data[i]
-            cnt += 1
-    
-    # 공유기가 충분히 설치되었다면
-    if cnt >= c :
+    for x in data[1:] :
+        # 만약 인접한 두 공유기 사이의 거리가 충분하다면
+        if mid + now <= x :
+            total += 1
+            now = x
+            
+    # 공유기 설치 대수가 충분하다면
+    # 상한선 내리기
+    if total >= c :
         result = mid
         start = mid + 1
     else :
         end = mid - 1
-        
+
 print(result)
