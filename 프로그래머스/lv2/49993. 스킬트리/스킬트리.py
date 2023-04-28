dfa8cdef90f6c29f6471f94567083c42dd078634
GinToFim <shlf6673@gmail.com>
2023-04-28 20:06:00 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,30 +1,28 @@
-# 아이디어 : 
-# 알고리즘 : 
-# 자료구조 : queue(deque) 사용
+# 아이디어 : 1. skill 의 문자들을 queue에 저장
+#           2. skill_tree에서 만약 queue 안에 있는 ch가 나왔는데
+#               A. queue[0]가 같지않다면 무시
+#               B. 같다면 queue.popleft() 
+# 알고리즘 : FIFO
+# 자료구조 : queue (deque)
 
 from collections import deque
 
 def solution(skill, skill_trees):
     answer = 0
-    
+
     for skill_tree in skill_trees :
-        queue = deque(skill)
-        v = queue.popleft()
-        check = True
-        idx = 0
+        skill_q = deque(ch for ch in skill)
+        check = True # 가능한 스킬트리인지 확인
         
-        # 큐가 빌 때까지
-        while queue and idx < len(skill_tree):
-            if v != skill_tree[idx] :
-                if skill_tree[idx] in queue : 
+        for ch in skill_tree :
+            if ch in skill_q :
+                if ch != skill_q[0] :
                     check = False
                     break
-            else :
-                v = queue.popleft()
-
-            idx += 1
-
+                else :
+                    skill_q.popleft()
+        
         if check :
             answer += 1
-            
+    
     return answer
