0af4e96af3e63a134a87434629cafb13d458e261
GinToFim <shlf6673@gmail.com>
2023-05-07 23:02:32 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 60 ms, Memory: 5744 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -5,15 +5,16 @@
 
 using namespace std;
 
-int N, E, v1, v2, res = INF;
+int n, e;
+int v1, v2, res = INF;
 int sToV1, sToV2, V1ToV2, V1ToN, V2ToN;
 vector<pair<int, int> > v[MAX]; // v[a] = (b,c) : a에서 b까지 c의 거리로 이동 가능
-int dist[MAX];
+int dist_table[MAX];
 
 void dijk(int start)
 {
-	for (int i = 0; i <= N; i++) dist[i] = INF;
-	dist[start] = 0;
+	for (int i = 0; i <= n; i++) dist_table[i] = INF;
+	dist_table[start] = 0;
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
 	q.push({ 0,start }); // 현재까지 거리, 현재 위치
 	while (!q.empty()) {
@@ -23,9 +24,9 @@ void dijk(int start)
 		for (int i = 0; i < v[cur].size(); i++) {
 			int next = v[cur][i].first;
 			int nextDist = v[cur][i].second;
-			if (dist[next] > curDist + nextDist) {
-				dist[next] = curDist + nextDist;
-				q.push({ dist[next],next });
+			if (dist_table[next] > curDist + nextDist) {
+				dist_table[next] = curDist + nextDist;
+				q.push({ dist_table[next],next });
 			}
 		}
 	}
@@ -33,25 +34,29 @@ void dijk(int start)
 
 int main()
 {
-	cin >> N >> E;
-	while (E--) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+	cin >> n >> e;
+	for (int i = 0; i < e; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		v[a].push_back({ b,c });
 		v[b].push_back({ a,c });
 	}
+    
 	cin >> v1 >> v2;
 
 	dijk(1);
-	sToV1 = dist[v1];
-	sToV2 = dist[v2];
+	sToV1 = dist_table[v1];
+	sToV2 = dist_table[v2];
 
 	dijk(v1);
-	V1ToV2 = dist[v2];
-	V1ToN = dist[N];
+	V1ToV2 = dist_table[v2];
+	V1ToN = dist_table[n];
 
 	dijk(v2);
-	V2ToN = dist[N];
+	V2ToN = dist_table[n];
 
 
 	res = min(res, sToV1 + V1ToV2 + V2ToN);
