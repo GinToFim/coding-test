6154f6d0d3c2816ac844d094604ea609996e3c86
GinToFim <shlf6673@gmail.com>
2023-05-09 01:45:28 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 60 ms, Memory: 5740 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,66 +1,83 @@
+/*
+아이디어 : 1. (1 -> v1 -> v2 -> n) VS (1 -> v2 -> v1 -> n) 비교하기
+알고리즘 : dijkstra(graph, dist_table, INF)
+자료구조 : pq 
+*/
+
 #include <bits/stdc++.h>
 
-#define INF 1e9
 #define MAX 801
+#define INF 1e7
 
 using namespace std;
 
 int n, e;
-int v1, v2, res = INF;
-int sToV1, sToV2, V1ToV2, V1ToN, V2ToN;
-vector<pair<int, int> > v[MAX]; // v[a] = (b,c) : a에서 b까지 c의 거리로 이동 가능
-int dist_table[MAX];
+vector<pair<int, int> > graph[MAX];
+int v1, v2;
 
-void dijk(int start)
-{
-	for (int i = 0; i <= n; i++) dist_table[i] = INF;
-	dist_table[start] = 0;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-	q.push({ 0,start }); // 현재까지 거리, 현재 위치
-	while (!q.empty()) {
-		int cur = q.top().second;
-		int curDist = q.top().first;
-		q.pop();
-		for (int i = 0; i < v[cur].size(); i++) {
-			int next = v[cur][i].first;
-			int nextDist = v[cur][i].second;
-			if (dist_table[next] > curDist + nextDist) {
-				dist_table[next] = curDist + nextDist;
-				q.push({ dist_table[next],next });
-			}
-		}
-	}
+int dijkstra(int start, int end) {
+    // 거리 테이블 정의
+    int dist_table[MAX];
+    fill(dist_table, dist_table + MAX, INF);
+    
+    priority_queue<pair<int, int> > pq;
+    pq.push({0, start}); // {거리, 노드} 순으로 저장
+    dist_table[start] = 0;
+    
+    // pq가 빌 때까지
+    while(!pq.empty()) {
+        int dist = -pq.top().first;
+        int now = pq.top().second;
+        pq.pop();
+        
+        // 이미 처리된 적이 있다면
+        if (dist_table[now] < dist) continue;
+        
+        // 해당 노드와 인접한 노드들 확인
+        for (int i = 0; i < graph[now].size(); i++) {
+            int v = graph[now][i].first;
+            int d = graph[now][i].second;
+            
+            int cost = dist + d;
+            
+            // 갱신 거리가 기존 거리보다 짧다면
+            if (cost < dist_table[v]) {
+                dist_table[v] = cost;
+                pq.push({-cost, v});
+            }
+        }
+        
+    }
+    return dist_table[end];
 }
 
-int main()
-{
+int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-	cin >> n >> e;
-	for (int i = 0; i < e; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		v[a].push_back({ b,c });
-		v[b].push_back({ a,c });
-	}
+    cin >> n >> e;
     
-	cin >> v1 >> v2;
-
-	dijk(1);
-	sToV1 = dist_table[v1];
-	sToV2 = dist_table[v2];
-
-	dijk(v1);
-	V1ToV2 = dist_table[v2];
-	V1ToN = dist_table[n];
-
-	dijk(v2);
-	V2ToN = dist_table[n];
-
-
-	res = min(res, sToV1 + V1ToV2 + V2ToN);
-	res = min(res, sToV2 + V1ToV2 + V1ToN);
-	if (V1ToV2 == INF || res == INF) cout << -1;
-	else cout << res;
+    // 간선 정보 입력
+    for(int i = 0; i < e; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        // 양방향
+        graph[a].push_back({b, c});
+        graph[b].push_back({a, c});
+    }
+    
+    cin >> v1 >> v2;
+    
+    // 1 -> v1 -> v2 -> n
+    int value1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n);
+    int value2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n);
+    
+    int result = min(value1, value2);
+    
+    if (result < INF)
+        cout << result << '\n';
+    else 
+        cout << -1 << '\n';
+    
+    return 0;
 }
