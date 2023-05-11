81bd4cc73c94ce160ab816407484e33304e224fb
GinToFim <shlf6673@gmail.com>
2023-05-11 19:45:45 +0900

    [Silver V] Title: 영화감독 숌, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,37 @@
+/*
+아이디어 :
+알고리즘 : 브루트 포스
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	
+	int num = 0;
+	int cnt = 0;
+	
+	while (cnt < n) {
+		num++;
+		int tmp = num;
+		
+		while (tmp > 0) {
+			if (tmp % 1000 == 666) {
+				cnt++;
+				break;
+			}
+			tmp /= 10;
+		}
+	}
+	
+	cout << num << '\n';
+	
+	return 0;
+}
