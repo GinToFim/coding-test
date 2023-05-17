b5da538837f95b57ff737e90b58b898e86127fbe
GinToFim <shlf6673@gmail.com>
2023-05-17 14:15:17 +0900

    [level 3] Title: 입국심사, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,20 +1,22 @@
-# 아이디어 : 이 정도 시간이 주어지면 충분한가? yes or no?
-# 알고리즘 : binary search(O(logn) = 10 * 10 * 10 = 10^3)
-#           parametric search
+# 아이디어 : 1. 이 정도 시간이면 충분한가?
+#           2. start : 0, end : max(times) * n
+# 알고리즘 : 10^9 -> 이진 탐색, 파라메트릭 서치
+
 
 def solution(n, times):
     answer = 0
-    
     start, end = 0, max(times) * n
     
+    # mid는 심사가 걸리는 시간
     while start <= end :
         mid = (start + end) // 2
-        total = 0 # 통과된 사람 수
         
-        for time in times :
-            total += mid // time
-
-        # 충분하다면(-> 상한선 줄이기)
+        total = 0 # 심사한 인원 수
+        
+        for t in times :
+            total += mid // t
+        
+        # 심사한 인원 수가 충분하다면, 상한선 줄이기
         if total >= n :
             answer = mid
             end = mid - 1
