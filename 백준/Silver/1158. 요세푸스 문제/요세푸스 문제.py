af99d168da2d119a243149be1ad83ee3406f59aa
GinToFim <shlf6673@gmail.com>
2023-05-18 15:11:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,24 +1,14 @@
-# 아이디어 : 1. k번만큼 넣었다가 빼기
-# 알고리즘 : 
-# 자료구조 : queue(deque)
-
-from collections import deque
-
 n, k = map(int, input().split())
 
-queue = deque(x for x in range(1, n + 1))
-result = []
+# 인덱스를 맞추기 위하여 0도 추가
+data = [x for x in range(1, n + 1)]
 
-# queue가 빌 때까지
-while queue :
-    # k-1번 만큼 넣었다가 빼기
-    for _ in range(k - 1) :
-        queue.append(queue.popleft())
-    
-    now = queue.popleft()
-    result.append(now)
-    
-print("<", end="")
-for x in result[:-1] :
-    print(x, end=', ')
-print(f"{result[-1]}>")
+cnt = 0 # data를 pop할 인덱스 순서
+result = [] # 요세푸스 결과 리스트
+
+# 리스트가 빌 때까지
+while data :
+    cnt = (cnt + k - 1) % len(data)
+    result.append(data.pop(cnt))
+
+print("<" + ", ".join(map(str, result)) + ">")
