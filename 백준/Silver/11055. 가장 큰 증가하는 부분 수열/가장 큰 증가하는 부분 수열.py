b9a18ce927c4030b3804b3085922a63b107bd244
GinToFim <shlf6673@gmail.com>
2023-05-18 16:45:31 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 172 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -1,13 +1,18 @@
+# 아이디어
+# 알고리즘 : LIS - DP (n = 1000 -> n^2 = 10^6 )
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 data = list(map(int, input().split()))
-dp = [x for x in data] # dp 테이블 초기화
 
-for i in range(n) :
+# 해당 원소로 dp 테이블 갱신
+dp = [x for x in data]
+
+for i in range(1, n) :
     for j in range(i) :
         if data[i] > data[j] :
             dp[i] = max(dp[i], dp[j] + data[i])
-
+            
 print(max(dp))
