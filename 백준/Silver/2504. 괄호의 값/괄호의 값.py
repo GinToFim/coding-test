d9fb6c517d7ff5fd9bef8a9b6d9d22dd299f83c7
GinToFim <shlf6673@gmail.com>
2023-05-23 17:51:48 +0900

    [Silver I] Title: 괄호의 값, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.py
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -1,56 +1,48 @@
-# 아이디어 : 1. '('이면 stack에 요소를 담고 tmp * 2
-#           2. ')'일 때,
-#                a. stack이 빈 리스트이거나, stack의 top이 '('가 아니라면 0
-#                b. bracket의 바로 전이 '('이면 result에 더하기
-#                c. 아니라면 통과
-#                d. 다시 tmp //= 2
-# 알고리즘 : X
-# 자료구조 : stack (list)
+# 아이디어 : 1. 열린 괄호라면 stack에 push, 해당 수만큼 tmp에 곱하기
+#           2. 닫힌 괄호라면
+#              a. 바로 앞이 해당 열린 괄호라면 result에 추가
+#              b. 아니라면 나누기
+# 자료구조 : 스택
 
-import sys
-input = sys.stdin.readline
+bracket = input()
 
-bracket = input().rstrip()
+stack = []
+result = 0
+now = 1
 
-stack = [] # 괄호 상황을 담을 리스트
-result = 0 # 결과 
-tmp = 1 # 초기값
 
-for i in range(len(bracket)) :
-    # 여는 괄호이면
+for i in range(len(bracket)):
+    # 열린 괄호라면
     if bracket[i] == '(':
-        tmp *= 2
+        now *= 2
         stack.append('(')
     elif bracket[i] == '[':
-        tmp *= 3
+        now *= 3
         stack.append('[')
-    
-    # 닫는 괄호이면
+    # 닫힌 괄호라면
     elif bracket[i] == ')':
-        # 빈 스택이거나 top이 여는 괄호가 아니라면
+        # stack 안에 원소가 없으면서 top이 해당 열린 괄호가 아니라면
         if not stack or stack[-1] != '(' :
             result = 0
             break
-        
-        # 이전 원소가 해당 여는 괄호라면
-        if bracket[i-1] == '(' : 
-            result += tmp
-        tmp //= 2
+        # 바로 이전이 열린괄호라면
+        if bracket[i-1] == '(': 
+            result += now
+
+        now //= 2
         stack.pop()
     else :
-        # 빈 스택이거나 top이 여는 괄호가 아니라면
         if not stack or stack[-1] != '[' :
             result = 0
             break
-        
-        # 이전 원소가 해당 여는 괄호라면
-        if bracket[i-1] == '[' : 
-            result += tmp
-        tmp //= 3
+        if bracket[i-1] == '[': 
+            result += now
+
+        now //= 3
         stack.pop()
     
-# 스택이 빈 리스트가 아니라면
-if stack :
+# 스택에 원소가 있다면
+if stack : 
     print(0)
 else :
     print(result)
