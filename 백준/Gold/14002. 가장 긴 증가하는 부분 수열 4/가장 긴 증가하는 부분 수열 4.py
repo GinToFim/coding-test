094b385c84f4c02578c5ad063aff90d010627f1e
GinToFim <shlf6673@gmail.com>
2023-05-25 16:58:32 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 44 ms, Memory: 33320 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,29 +1,33 @@
 # 아이디어 : 
-# 알고리즘 : LIS - DP (n = 1,000 -> n^2 = 10^6)
+# 알고리즘 : 
 
+import bisect
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 data = list(map(int, input().split()))
 
-# dp 테이블을 1로 초기화
-dp = [1 for _ in range(n)]
+dist = [0]
+index = [0 for _ in range(n+1)]
 
-for i in range(1, n) :
-    for j in range(i) :
-        if data[i] > data[j] :
-            dp[i] = max(dp[i], dp[j] + 1)
+for i in range(n):
+    x = data[i]
+    
+    if dist[-1] < x:
+        dist.append(x)
+        index[i] = len(dist) - 1
+    else:
+        index[i] = bisect.bisect_left(dist, x)
+        dist[index[i]] = x
 
-order = max(dp)
-print(order)
+length = len(dist)-1
+print(length)
 
-max_idx = dp.index(order)
-lis = []
+result = []
+for i in range(n, -1, -1):
+    if index[i] == length:
+        result.append(data[i])
+        length -= 1
 
-for i in range(max_idx, -1, -1) :
-    if dp[i] == order :
-        lis.append(data[i])
-        order -= 1
-
-print(*lis[::-1])
+print(*result[::-1])
