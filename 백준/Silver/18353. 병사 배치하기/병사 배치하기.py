f5a41898c08b02792a3f0190329cd76c451a2960
GinToFim <shlf6673@gmail.com>
2023-05-25 15:46:54 +0900

    [Silver II] Title: 병사 배치하기, Time: 48 ms, Memory: 33320 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,17 +1,21 @@
-# 아이디어 : 1. 가장 긴 감소하는 수열
-# 알고리즘 : LIS - dp (n = 2000 -> n^2=4*10^6)
+# 아이디어 : LDS -> LIS 변경
+# 알고리즘 : LIS - Binary Search
 
+import bisect
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 data = list(map(int, input().split()))
+data.reverse() # 배열 뒤집기
 
-dp = [1 for _ in range(n)]
+dp = [data[0]]
 
-for i in range(1, n):
-    for j in range(i):
-        if data[i] < data[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
-            
-print(n - max(dp))
+for x in data[1:]:
+    if dp[-1] < x :
+        dp.append(x)
+    else:
+        idx = bisect.bisect_left(dp, x)
+        dp[idx] = x
+
+print(n - len(dp))
