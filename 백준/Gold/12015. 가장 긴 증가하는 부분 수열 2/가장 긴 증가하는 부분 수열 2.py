3bd771e03da89912da82fe5b3650727583573cf6
GinToFim <shlf6673@gmail.com>
2023-05-26 20:09:20 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 908 ms, Memory: 157628 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,5 +1,5 @@
-# 아이디어
-# 알고리즘 : LIS - 이진 탐색
+# 아이디어 : 
+# 알고리즘 : LCS - binary search
 
 import bisect
 import sys
@@ -11,7 +11,7 @@ data = list(map(int, input().split()))
 dp = [data[0]]
 
 for x in data[1:]:
-    if x > dp[-1]:
+    if dp[-1] < x :
         dp.append(x)
     else :
         idx = bisect.bisect_left(dp, x)
