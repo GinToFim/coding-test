a1c9e1624f628471e01cb845fbe9b011ab9f7947
GinToFim <shlf6673@gmail.com>
2023-05-29 16:29:58 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,50 +1,37 @@
-import sys
+# 아이디어 : 
+# 알고리즘 : 브루트 포스
 
+import sys
 input = sys.stdin.readline
 
+BW = "BW" * 8
+BW_board = []
+for i in range(8):
+    BW_board.append(BW[i:i + 8])
 
-# wb로 시작하는 것에 대하여 다시 칠하는 개수
-def wb_cnt(x, y):
-    cnt = 0
-    for i in range(8):
-        for j in range(8):
-            if board[i + x][j + y] != wb[i][j]:
-                cnt += 1
-    return cnt
-
-
-# bw로 시작하는 것에 대하여 다시 칠하는 개수
-def bw_cnt(x, y):
-    cnt = 0
-    for i in range(8):
-        for j in range(8):
-            if board[i + x][j + y] != bw[i][j]:
-                cnt += 1
-    return cnt
-
+WB = "WB" * 8
+WB_board = []
+for i in range(8):
+    WB_board.append(WB[i:i + 8])
 
 n, m = map(int, input().split())
+board = [input().rstrip() for _ in range(n)]
+    
+def color_cnt(color_board, x, y):
+    cnt = 0
+    
+    for i in range(8):
+        for j in range(8):
+            if board[i+x][j+y] != color_board[i][j]:
+                cnt += 1
+    
+    return cnt
 
-# 보드 입력받기
-board = []
-for _ in range(n):
-    board.append(input().rstrip())
 
-wb = [
-    "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW",
-    "WBWBWBWB", "BWBWBWBW"
-]
-
-bw = [
-    "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB",
-    "BWBWBWBW", "WBWBWBWB"
-]
-
-result = 1e7
-
+result = 1e9
 for i in range(n - 7):
     for j in range(m - 7):
-        tmp = min(wb_cnt(i, j), bw_cnt(i, j))
+        tmp = min(color_cnt(BW_board, i, j), color_cnt(WB_board, i, j))
         result = min(result, tmp)
-
+        
 print(result)
