85247bb5817b369acd1aacba7b4334c7a5243d59
GinToFim <shlf6673@gmail.com>
2023-06-01 16:28:46 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,3 +1,6 @@
+# 아이디어
+# 알고리즘 : dfs, back tracking
+
 import sys
 input = sys.stdin.readline
 
@@ -5,36 +8,30 @@ n = int(input())
 data = list(map(int, input().split()))
 add, sub, mul, div = map(int, input().split())
 
-max_value = -int(1e9)
-min_value = int(1e9)
+max_value = -1e9
+min_value = 1e9
 
-def dfs(idx, num) :
-	global max_value, min_value, add, sub, mul, div
-
-	if idx == n :
-		max_value = max(max_value, num)
-		min_value = min(min_value, num)
-		return 
-
-	if add > 0 :
-		add -= 1
-		dfs(idx + 1, num + data[idx])
-		add += 1
-	if sub > 0 :
-		sub -= 1
-		dfs(idx + 1, num - data[idx])
-		sub += 1
-	if mul > 0 :
-		mul -= 1
-		dfs(idx + 1, num * data[idx])
-		mul += 1
-	if div > 0 :
-		div -= 1
-		dfs(idx + 1, int(num / data[idx]))
-		div += 1
-
-# dfs 호출
-dfs(1, data[0])
-		
+def dfs(depth, num, add, sub, mul, div):
+    global max_value, min_value
+    
+    # 종료조건
+    if depth == n:
+        max_value = max(max_value, num)
+        min_value = min(min_value, num)
+        return
+    
+    if add > 0 :
+        dfs(depth + 1, num + data[depth], add - 1, sub, mul, div)
+        
+    if sub > 0 :
+        dfs(depth + 1, num - data[depth], add, sub - 1, mul, div)
+        
+    if mul > 0 :
+        dfs(depth + 1, num * data[depth], add, sub, mul - 1, div)
+        
+    if div > 0 :
+        dfs(depth + 1, int(num / data[depth]), add, sub, mul, div - 1)
+        
+dfs(1, data[0], add, sub, mul, div)
 print(max_value)
 print(min_value)
