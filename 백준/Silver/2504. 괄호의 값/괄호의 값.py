6897052d73063b6c8096054f378c47130316883b
GinToFim <shlf6673@gmail.com>
2023-06-04 17:24:05 +0900

    [Silver I] Title: 괄호의 값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.py
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -1,48 +1,57 @@
-# 아이디어 : 1. 열린 괄호라면 stack에 push, 해당 수만큼 tmp에 곱하기
-#           2. 닫힌 괄호라면
-#              a. 바로 앞이 해당 열린 괄호라면 result에 추가
-#              b. 아니라면 나누기
-# 자료구조 : 스택
+# 아이디어 : 1. 열린 괄호가 나오면 곱하기
+#           2. 닫힌 괄호가 나오면 
+#                첫 번째 닫힌 괄호면 result에 더하고 나누기
+#                두 번째 이후 닫힌 괄호면 나누기
+#           3. stack의 top의 닫힌괄호와 열린괄호가 맞지 않으면 0
+#              for 문을 다 돌고 stack의 요소가 남아 있으면 0
+# 알고리즘 : implementation
+# 자료구조 : stack - 
 
-bracket = input()
+import sys
+input = sys.stdin.readline
 
-stack = []
+brackets = input().rstrip()
+
 result = 0
 now = 1
+stack = []
 
-
-for i in range(len(bracket)):
-    # 열린 괄호라면
-    if bracket[i] == '(':
+for i in range(len(brackets)):
+    # 열린 괄호일 때
+    if brackets[i] == '(':
         now *= 2
         stack.append('(')
-    elif bracket[i] == '[':
+    elif brackets[i] == '[':
         now *= 3
         stack.append('[')
-    # 닫힌 괄호라면
-    elif bracket[i] == ')':
-        # stack 안에 원소가 없으면서 top이 해당 열린 괄호가 아니라면
-        if not stack or stack[-1] != '(' :
+    # 닫힌 괄호일 때
+    elif brackets[i] == ')':
+        # stack의 요소가 없거나 stack의 top이 열린 괄호가 아니라면
+        if len(stack) <= 0 or stack[-1] != '(':
             result = 0
             break
-        # 바로 이전이 열린괄호라면
-        if bracket[i-1] == '(': 
+        
+        # 바로 이전이 열린 괄호이면
+        if brackets[i-1] == '(':
             result += now
-
+        
+        stack.pop()
         now //= 2
-        stack.pop()
-    else :
-        if not stack or stack[-1] != '[' :
+    else:
+        # stack의 top이 열린 괄호가 아니라면
+        if len(stack) <= 0 or stack[-1] != '[':
             result = 0
             break
-        if bracket[i-1] == '[': 
+        
+        # 바로 이전이 열린 괄호이면
+        if brackets[i-1] == '[':
             result += now
-
-        now //= 3
+        
         stack.pop()
+        now //= 3
+
+# stack의 요소가 남아있다면
+if len(stack) > 0 :
+    result = 0
     
-# 스택에 원소가 있다면
-if stack : 
-    print(0)
-else :
-    print(result)
+print(result)
