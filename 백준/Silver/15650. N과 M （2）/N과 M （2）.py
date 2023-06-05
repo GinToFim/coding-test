e4ec206fd13eb07922baa54fe16914952830b4ad
GinToFim <shlf6673@gmail.com>
2023-06-05 16:54:03 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,16 @@
-# 백트래킹 정의
-def dfs(num, start) :
+n, m = map(int, input().split())
+
+result = []
+
+def dfs(num, start):
+    # 종료 조건
     if num == m :
         print(*result)
         return
     
-    for i in range(start, n + 1) :
+    for i in range(start, n + 1):
         result.append(i)
         dfs(num + 1, i + 1)
         result.pop()
-            
-
-n, m = map(int, input().split())
-result = []
-visited = [False] * (n + 1)
-
-# 백트래킹 실행
+        
 dfs(0, 1)
