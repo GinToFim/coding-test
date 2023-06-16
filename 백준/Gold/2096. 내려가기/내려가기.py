d19d5542d0b79fb6ad7a8b9484c9d453c37bba44
GinToFim <shlf6673@gmail.com>
2023-06-16 22:03:51 +0900

    [Gold V] Title: 내려가기, Time: 300 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,28 +1,15 @@
-# 아이디어
-# 알고리즘 : dp
-
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-max_prev = list(map(int, input().split()))
-min_prev = [x for x in max_prev]
+max_dp = list(map(int, input().split()))
+min_dp = [x for x in max_dp]
 
-max_now = [0 for _  in range(3)]
-min_now = [0 for _  in range(3)]
-
-for _ in range(n-1) :
+for _ in range(n - 1):
     a, b, c = map(int, input().split())
     
-    max_now[0] = a + max(max_prev[0], max_prev[1])
-    max_now[1] = b + max(max_prev)
-    max_now[2] = c + max(max_prev[1], max_prev[2])
-    
-    min_now[0] = a + min(min_prev[0], min_prev[1])
-    min_now[1] = b + min(min_prev)
-    min_now[2] = c + min(min_prev[1], min_prev[2])
-    
-    max_prev = [x for x in max_now]
-    min_prev = [x for x in min_now]
-    
-print(max(max_prev), min(min_prev))
+    # dp 배열 값을 갱신
+    max_dp = [a + max(max_dp[0], max_dp[1]), b + max(max_dp), c + max(max_dp[1], max_dp[2])]
+    min_dp = [a + min(min_dp[0], min_dp[1]), b + min(min_dp), c + min(min_dp[1], min_dp[2])]
+
+print(max(max_dp), min(min_dp))
