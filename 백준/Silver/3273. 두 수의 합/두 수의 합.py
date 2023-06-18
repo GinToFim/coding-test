3262b5e0bb97671d917c0e49e65abd8163c0fa97
GinToFim <shlf6673@gmail.com>
2023-06-18 22:16:07 +0900

    [Silver III] Title: 두 수의 합, Time: 104 ms, Memory: 42176 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,19 +1,30 @@
+# 아이디어 : 1. 리스트 오름차순 정렬하기
+#           2. start, end를 0, n - 1로 설정
+#           3. 합이 x보다 작다면 start += 1
+#              합이 x보다 크거나 같다면 end -= 1
+# 알고리즘 : 투 포인터
+
 import sys
 input = sys.stdin.readline
 
-if __name__ == '__main__':
-    N = int(input())
-    arr = list(map(int, sys.stdin.readline().split()))
-    X = int(input())
-    arr.sort()
-    left, right = 0, N - 1
-    ans = 0
+n = int(input())
+data = list(map(int, input().split()))
+x = int(input())
 
-    while left < right:
-        tmp = arr[left] + arr[right]
-        if tmp == X: ans += 1
-        if tmp < X:
-            left += 1
-            continue
-        right -= 1
-    print(ans)
+data.sort() # 오름차순 정렬
+
+start, end = 0, n - 1
+result = 0
+
+while start < end :
+    hap = data[start] + data[end]
+    
+    if hap == x :
+        result += 1
+        end -= 1
+    elif hap < x :
+        start += 1
+    else :
+        end -= 1
+
+print(result)
