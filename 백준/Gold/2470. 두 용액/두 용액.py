bc844a9dca0c3b4242a970dd46efc95b9464ae42
GinToFim <shlf6673@gmail.com>
2023-06-19 17:31:50 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42176 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,5 +1,8 @@
-# 아이디어 : 
-# 알고리즘 : 
+# 아이디어 : 1. 오름차순 정렬
+#           2. 두 용액의 절댓값 합이 음수일 때 start를 증가시키면 합 증가
+#              두 용액의 절댓값 합이 양수일 때 end를 감소시키면 합 감소
+#           3. 0이면 바로 종료
+# 알고리즘 : sort, two pointer
 
 import sys
 input = sys.stdin.readline
@@ -7,29 +10,25 @@ input = sys.stdin.readline
 n = int(input())
 data = list(map(int, input().split()))
 
-data.sort() # 오름차순 정렬
-start, end = 0, n - 1
+# 오름차순 정렬
+data.sort()
 
+start, end = 0, n - 1
 result = abs(data[start] + data[end])
-final = [data[start], data[end]]
+two_values = (data[start], data[end])
 
 while start < end :
-    # 절댓값 합 계산
     hap = data[start] + data[end]
     
-    # 갱신한 절대값이 더 작다면
     if abs(hap) < result :
         result = abs(hap)
-        final = [data[start], data[end]]
-        # 0 이 나왔다면 바로 종료
+        two_values = (data[start], data[end])
         if result == 0 :
             break
     
-    # 합이 음수가 나왔다면
     if hap < 0 :
         start += 1
-    # 합이 양수가 나왔다면
     else :
         end -= 1
-
-print(*final)
+        
+print(*two_values)
