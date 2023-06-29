367741228810013564cf7eeab823536be5e5416b
GinToFim <shlf6673@gmail.com>
2023-06-29 19:04:21 +0900

    [Silver IV] Title: 수들의 합 2, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,25 +1,28 @@
-# 아이디어
-# 알고리즘 : 누적합
+# 아이디어 : 
+# 알고리즘 : prefix sum, two pointer
 
-from itertools import combinations
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
 
-dp = [0]
+# 누적합 초기화
+dp = [0] 
 sum_value = 0
 
-for x in data:
+for x in data :
     sum_value += x
     dp.append(sum_value)
-
+    
+start, end = 0, 0
 result = 0
 
-for iters in combinations(range(n+1), 2):
-    left, right = iters
-    if (dp[right] - dp[left]) == m:
+for start in range(n+1):
+    while end < n and (dp[end] - dp[start]) < m :
+        end += 1
+    
+    if (dp[end] - dp[start]) == m :
         result += 1
-        
+    
 print(result)
