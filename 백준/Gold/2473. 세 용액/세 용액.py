ce6a15b2d1eeb602b4765291f0d3bcec9e31637f
GinToFim <shlf6673@gmail.com>
2023-06-29 18:13:08 +0900

    [Gold III] Title: 세 용액, Time: 2060 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,7 +1,7 @@
-# 아이디어 : 1. start, end는 움직이되, 나머지 하나는 완전탐색
-#           
-# 알고리즘 : brute force + two pointer
-#           시간복잡도 = O(n^2) = 25,000,000
+# 아이디어 : 1. 오름차순 정렬
+#            2. for문으로 맨 앞 원소 하나씩 뽑기
+#            3. 나머지 배열을 투 포인터를 이용하여 2개 뽑기
+# 알고리즘 : two pointer, sort, brute force
 
 import sys
 input = sys.stdin.readline
@@ -9,28 +9,33 @@ input = sys.stdin.readline
 n = int(input())
 data = list(map(int, input().split()))
 
-data.sort() # 오름차순 정렬
+# 오름차순 정렬
+data.sort()
 
-min_value = 4 * int(1e9)
+# 맨 앞 하나만 뽑기
+def two_pointer(data) :
+    min_value = 4 * int(1e9)
+    for i in range(n-2):
+        start, end = i + 1, n - 1
 
-for i in range(n-2):
-    start, end = i+1, n-1
+        # 투 포인터 알고리즘 실행
+        while start < end :
+            hap = data[i] + data[start] + data[end]
+
+            if abs(hap) < min_value :
+                min_value = abs(hap)
+                three_values = [data[i], data[start], data[end]]
+
+                if hap == 0 :
+                    return three_values
+
+            # hap 이 음수라면
+            if hap < 0 :
+                start += 1
+            else :
+                end -= 1
     
-    while start < end :
-        hap = data[i] + data[start] + data[end]
-        
-        if abs(hap) < min_value:
-            min_value = abs(hap)
-            result = (data[i], data[start], data[end])
-            
-            if min_value == 0 :
-                break
-        
-        # 음수라면 start 증가
-        if hap < 0 :
-            start += 1
-        # 양수라면 end 감소
-        else :
-            end -= 1
-            
+    return three_values
+                    
+result = two_pointer(data)
 print(*result)
