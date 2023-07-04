12c11722fdd51c585ab77cbc20531604840f4f20
GinToFim <shlf6673@gmail.com>
2023-07-04 20:15:52 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,5 +1,5 @@
-# 아이디어 : 1. p[0] + s[0] * days >= 100 이면 pop하기
-#           2. pop에 대한 개수만큼 완성인 상태 늘리기
+# 아이디어 : 1. 두 리스트를 큐(덱)로 변경
+#           2. 
 # 알고리즘 : 
 # 자료구조 : queue(deque)
 
@@ -7,25 +7,25 @@ from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    days = 1 # 날짜
-    cnt = 0 # 배포 개수
-    
     progresses = deque(progresses)
     speeds = deque(speeds)
+    day = 1 # 현재 날짜
+    cnt = 0 # 배포 기능 개수
     
-    # progresses가 빌 때까지
     while progresses :
-        if progresses[0] + speeds[0] * days >= 100 :
-            progresses.popleft()
-            speeds.popleft()
-            cnt += 1
-        else :
-            if cnt > 0 :
-                answer.append(cnt)
-                cnt = 0
-            days += 1
-    
-    # 남은 배포 append
-    answer.append(cnt)
+        # 배포가 가능하다면
+        while len(progresses) > 0 : 
+            if progresses[0] + day * speeds[0] >= 100 :
+                progresses.popleft()
+                speeds.popleft()
+                cnt += 1
+            else :
+                break
+        
+        # 배포할 기능이 있다면
+        if cnt > 0 :
+            answer.append(cnt)
+            cnt = 0
+        day += 1
     
     return answer
