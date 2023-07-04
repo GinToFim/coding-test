39301a5012b3384dd9b24475eefc1b547830d800
GinToFim <shlf6673@gmail.com>
2023-07-04 16:45:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 66.03 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,19 +1,9 @@
-# arr는 queue, answer는 stack으로
-
-from collections import deque
-
 def solution(arr):
     answer = []
-    queue = deque(arr)
+    answer.append(arr[0])
     
-    # 첫 번째는 요소는 그냥 추가
-    answer.append(queue.popleft())
+    for x in arr[1:] :
+        if answer[-1] != x :
+            answer.append(x)
     
-    # queue가 빌 때까지
-    while queue :
-        v = queue.popleft()
-        # stack의 top과 다르다면 append
-        if answer[-1] != v :
-            answer.append(v)
-        
     return answer
