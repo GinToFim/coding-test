e2f26c82282b7ddb5203aeb367bec588fdac46fd
GinToFim <shlf6673@gmail.com>
2023-07-04 16:33:15 +0900

    [level 2] Title: 전화번호 목록, Time: 86.81 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,14 @@
-def solution(phone_book):
-    phone_book=sorted(phone_book) #String이므로 사전순으로 정렬된다.
-    for i in range(len(phone_book)-1): #인덱스로 반복문 돌리기 맨마지막 요소는 제외 
-        str = phone_book[i]
-        if(str==phone_book[i+1][:len(str)]): #앞서 맨 마지막 인덱스 제외 안 하면 여기서 오류 걸림 
-             return False
+# 아이디어 : 1. 오름차순 정렬
+#           2. 접두사 단방향으로 탐색
+# 알고리즘 : sorting (O(nlogn)) 
 
+def solution(phone_book):
+    # 오름차순 정렬
+    phone_book.sort()
+    
+    for i in range(len(phone_book) - 1) :
+        # 이전 단어가 다음 단어의 접두사라면
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    
     return True
