4c0ff0545054fc6a75173b3e6315182d19dac38b
GinToFim <shlf6673@gmail.com>
2023-07-06 17:56:29 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,41 +1,42 @@
-# 아이디어 : 1. second, bridge : 큐 사용
-#            2. bridge의 합과 truck_weights[0] 이 제한 무게보다 작으면
-#            3. second와 bridge 원소 추가
-# 자료구조 : queue(deque)
 
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    answer = 0 # 총 시간
-    
-    truck_weights = deque(truck_weights) # 다리를 건널 트럭들
-    bridge_second = deque() # 다리를 건너고 있는 시간들
-    bridge_weights = deque() # 다리 위에 무게
-    
-    # 맨 앞 트럭 한 대 추가하기
-    bridge_second.append(0)
-    bridge_weights.append(truck_weights.popleft())
-    
-    # 다리가 빌 때까지
-    while bridge_weights :
-        answer += 1
-        
-        # 1초씩 추가
-        bridge_second = deque(sec + 1 for sec in bridge_second)
-        
-        # 트럭이 도착했다면
-        if bridge_second[0] >= bridge_length :
-            bridge_second.popleft()
-            bridge_weights.popleft()
-        
-        # 남아있는 트럭이 있으면
-        if len(truck_weights) > 0 :
-            # 그리고 아직 다리의 무게가 여유가 있으면
-            if sum(bridge_weights) + truck_weights[0] <= weight :
-                bridge_second.append(0)
-                bridge_weights.append(truck_weights.popleft())
-    
-    # 끝나는 시간 1초 추가하기
-    answer += 1
-    
-    return answer
+    bridge = deque(0 for _ in range(bridge_length))
+    total_weight = 0
+    step = 0
+    truck_weights.reverse()
+
+    while truck_weights:
+        total_weight -= bridge.popleft()
+        if total_weight + truck_weights[-1] > weight:
+            bridge.append(0)
+        else:
+            truck = truck_weights.pop()
+            bridge.append(truck)
+            total_weight += truck
+        step += 1
+
+    step += bridge_length
+
+    return step
