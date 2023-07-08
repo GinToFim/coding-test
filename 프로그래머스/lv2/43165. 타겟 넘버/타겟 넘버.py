0052ab7d0475eac0d8fe07f21c8a967893d36e71
GinToFim <shlf6673@gmail.com>
2023-07-08 22:14:21 +0900

    [level 2] Title: 타겟 넘버, Time: 2.62 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,22 +1,24 @@
-# 아이디어 : 1. itertools의 product를 사용하여 중복순열 구현
-#           2. 1 or -1을 중복 순열 (len(numbers)만큼)
-# 알고리즘 : dfs(백트래킹) (2^20 = 1,000,000)
-# 자료구조 : stack
-
-from itertools import product
+# 아이디어 : 
+# 알고리즘 : dfs
+# 자료구조 : stack(recursion)
 
 def solution(numbers, target):
     answer = 0
     n = len(numbers)
     
-    operators_list = [iters for iters in product((-1, 1), repeat=n)]
-    
-    for operators in operators_list :
-        hap = 0
-        for op, num in zip(operators, numbers) :
-            hap += op * num
+    def dfs(depth, hap):
+        nonlocal answer
         
-        if hap == target :
-            answer += 1
+        # 종료조건
+        if depth == n:
+            if hap == target :
+                answer += 1
+            
+            return 
+        
+        dfs(depth + 1, hap + numbers[depth])
+        dfs(depth + 1, hap - numbers[depth])
+        
+    dfs(0, 0)
     
     return answer
