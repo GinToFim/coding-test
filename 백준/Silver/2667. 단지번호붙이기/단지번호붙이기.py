05d8f863c94a97b44b6a97304bb50eb3fac1d447
GinToFim <shlf6673@gmail.com>
2023-09-02 10:38:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,61 @@
+# 아이디어 : 1. 그래프, 상하좌우, 큐 정의
+#            2. bfs 탐색을 통하여 연결되어 있는 단지수 탐색
+#            3. 탐색된 단지수 오름차순 정렬
+# 알고리즘 : bfs
+# 자료구조 : queue(deque)
+
+from collections import deque
 import sys
 input = sys.stdin.readline
 
+# 그래프 입력
 n = int(input())
-graph = []
-for _ in range(n) :
-    graph.append(list(map(int, input().rstrip())))
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
-# 상하좌우
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+# 상하좌우 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def dfs(x, y) :
-	global cnt 
-	graph[x][y] = 0
-	cnt += 1
+# bfs 함수 정의
+def bfs(x, y):
+    # 큐 및 시작노드 정의
+    queue = deque()
+    queue.append((x, y))
+    graph[x][y] = 0
+    cnt = 1 # 연결되는 아파트의 수
+    
+    # 큐가 빌 때까지
+    while queue :
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            # 범위 밖이라면
+            if nx < 0 or ny < 0 or nx >= n or ny >= n :
+                continue
+                
+            # 아파트(1)라면
+            if graph[nx][ny] == 1 :
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
+                cnt += 1
+    
+    return cnt
 
-	for i in range(4) :
-		nx = x + dx[i]
-		ny = y + dy[i]
+# 단지의 아파트 수 리스트
+result = []
 
-		if nx <= -1 or nx >=n or ny <= -1 or ny >= n :
-			continue
-		if graph[nx][ny] == 1 : 
-			dfs(nx, ny)
-   
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 :
+            cnt = bfs(i, j)
+            result.append(cnt)
 
-result = 0 # 아파트 단지 수
-apt = [] # 아파트 단지 array
+# 오름차순 정렬
+result.sort()
 
-for i in range(n) :
-	for j in range(n) :
-		if graph[i][j] == 1:
-			cnt = 0 # 아파트 단지당 개수
-			dfs(i, j)
-			apt.append(cnt)
-			result += 1
-
-
-apt.sort()
-print(result)
-for x in apt :
+print(len(result))
+for x in result :
     print(x)
