1df1d5626a358598b78527009a19e5709bae0572
GinToFim <shlf6673@gmail.com>
2023-09-04 18:43:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,26 +1,22 @@
-# 아이디어 : '-'가 나오는 순간 전부 음수 처리
-# 알고리즘 : 그리디
-
 import sys
 input = sys.stdin.readline
 
-data = input().rstrip()
+exp = input().rstrip()
 
 result = 0
-flag = 1
 num = 0
+flag = 1
 
-for x in data :
-    if x == '-':
-        result += num * flag
-        flag = -1
-        num = 0
-    elif x == '+':
-        result += num * flag
-        num = 0
-    else :
+for x in exp :
+    if x.isdigit():
         num = num * 10 + int(x)
-
-# 마지막 더하기
+    else :
+        result += num * flag
+        num = 0
+        if x == '-':
+            flag = -1
+    
+# 마지막 숫자도 더하기
 result += num * flag
+    
 print(result)
