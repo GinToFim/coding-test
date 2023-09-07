0483e55b19ae196f381b445c0aa1e8b9ab0405b2
GinToFim <shlf6673@gmail.com>
2023-09-07 17:29:44 +0900

    [Silver III] Title: 파도반 수열, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,21 @@
+# 아이디어 : 
+# 알고리즘 : DP
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    
+    dp = [0 for _ in range(101)]
+    
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+    
+    for i in range(4, n+1):
+        dp[i] = dp[i-2] + dp[i-3]
+
+    print(dp[n])
