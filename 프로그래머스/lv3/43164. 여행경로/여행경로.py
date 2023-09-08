6ac1c5f2a17e91d9778e8231adcd2807ac1f7a43
GinToFim <shlf6673@gmail.com>
2023-09-08 19:53:02 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,20 +1,28 @@
-from collections import deque
+from collections import defaultdict
+
 def solution(tickets):
-    answer = []
-    q = deque()
-    q.append(("ICN",["ICN"], []))
+    src_dict = defaultdict(list)
     
-    while q:
-        airport, path, used = q.popleft()
-
-        if len(used) == len(tickets):
-            answer.append(path)
+    # src-dst를 key-[value]로 하는 dictionary
+    for ticket in tickets :
+        src, dst = ticket
+        src_dict[src].append(dst)
         
-        for idx, ticket in enumerate(tickets):
-            if ticket[0] == airport and not idx in used:
-                q.append((ticket[1], path+[ticket[1]], used+[idx]))
-                
+    # dict의 list들 내림차순 정렬
+    for src in src_dict:
+        src_dict[src].sort(reverse=True)
     
-    answer.sort()
-
-    return answer[0]
+    stack = ["ICN"]
+    path = []
+    
+    # stack이 빌 때까지
+    while stack:
+        top = stack[-1]
+        
+        # top을 출발지로 하는 것이 없거나 출발지가 다 비었을 때
+        if not src_dict[top] or len(src_dict[top]) == 0 :
+            path.append(stack.pop())
+        else :
+            stack.append(src_dict[top].pop())
+    
+    return path[::-1]
