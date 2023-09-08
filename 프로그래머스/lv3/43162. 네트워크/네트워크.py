16e23e2551eef9e922d1b32304f2d13bafef3d11
GinToFim <shlf6673@gmail.com>
2023-09-08 18:32:47 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,20 +1,29 @@
+# 아이디어 : 1. visited 테이블 생성
+#           2. 해당 노드를 한 번도 방문한 적이 없다면 dfs 탐색
+#           3. dfs에서 다른 노드를 방문한 적이 없으면서 연결되어 있다면
+#               재귀적으로 dfs 실행
+# 알고리즘 : dfs
+# 자료구조 : stack(recursion)
+
 def solution(n, computers):
     answer = 0
+    # visited 테이블 생성
     visited = [False for _ in range(n)]
     
     # dfs 함수 정의
     def dfs(graph, v, visited):
-        visited[v] = True # 해당 점 방문 처리
+        # 현재 노드는 방문 처리
+        visited[v] = True
         
         for i in range(n):
-            # 다른 점을 방문한 적이 없으면서 연결되어 있다면
+            # 해당 노드를 방문한 적이 없으면서 연결되어 있다면
             if not visited[i] and graph[v][i] == 1:
                 dfs(graph, i, visited)
                 
     for v in range(n):
-        # 해당 점을 방문한 적이 없다면
+        # 해당 노드를 방문한 적이 없다면 dfs 실행과 네트워크 추가
         if not visited[v]:
-            answer += 1
             dfs(computers, v, visited)
+            answer += 1
     
     return answer
