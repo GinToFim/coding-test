0e71d5a1745a8e9dd27807489a343af5c80803f0
GinToFim <shlf6673@gmail.com>
2023-09-08 17:29:45 +0900

    [level 2] Title: 타겟 넘버, Time: 2.66 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,24 +1,24 @@
-# 아이디어 : 
+# 아이디어 : dfs를 활용하여 경우의 수 구하기
 # 알고리즘 : dfs
-# 자료구조 : stack(recursion)
 
 def solution(numbers, target):
     answer = 0
     n = len(numbers)
     
-    def dfs(depth, hap):
+    def dfs(depth, num):
         nonlocal answer
         
-        # 종료조건
-        if depth == n:
-            if hap == target :
+        # 종료 조건
+        if depth == n :
+            if num == target :
                 answer += 1
-            
-            return 
+            return
         
-        dfs(depth + 1, hap + numbers[depth])
-        dfs(depth + 1, hap - numbers[depth])
+        # 더하기 빼기 정의
+        dfs(depth + 1, num + numbers[depth])
+        dfs(depth + 1, num - numbers[depth])
         
+    # 함수 실행
     dfs(0, 0)
     
     return answer
