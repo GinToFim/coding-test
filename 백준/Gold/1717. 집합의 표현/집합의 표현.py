cd55c49b5635f4f7feb6e18f13b859aec67181f8
GinToFim <shlf6673@gmail.com>
2023-10-05 19:54:42 +0900

    [Gold V] Title: 집합의 표현, Time: 456 ms, Memory: 120460 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,39 +1,36 @@
-# 아이디어
-# 알고리즘 : union-find
-# 자료구조 : disjoint set(union-find)
-
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
-# find 연산 (경로 압축)
+# find 연산 + 경로 압축
 def find_parent(parent, x) :
-    if parent[x] != x :
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+	if parent[x] != x :
+		parent[x] = find_parent(parent, parent[x])
+	return parent[x]
 
 # union 연산
 def union_parent(parent, a, b) :
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    
-    if a < b :
-        parent[b] = a
-    else :
-        parent[a] = b
+	a = find_parent(parent, a)
+	b = find_parent(parent, b)
+
+	if a < b :
+		parent[b] = a
+	else :
+		parent[a] = b
 
 n, m = map(int, input().split())
+parent = [0] * (n + 1)
 
-# 부모 테이블 초기화
-parent = [v for v in range(n + 1)]
+for i in range(1, n + 1) :
+	parent[i] = i
 
 for _ in range(m) :
-    op, a, b = map(int, input().split())
-    
-    if op == 0 :
-        union_parent(parent, a, b)
-    else :
-        if find_parent(parent, a) == find_parent(parent, b) :
-            print('YES')
-        else :
-            print('NO')
+	op, a, b = map(int, input().split())
+	# union 연산
+	if op == 0 :
+		union_parent(parent, a, b)
+	else :
+		if find_parent(parent, a) == find_parent(parent, b) :
+			print('YES')
+		else :
+			print('NO')
