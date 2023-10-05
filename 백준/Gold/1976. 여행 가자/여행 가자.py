f2c0114c45383252cc626942acf98ba6387b7aff
GinToFim <shlf6673@gmail.com>
2023-10-05 20:40:43 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,43 +1,47 @@
+# 아이디어
 # 알고리즘 : union-find
-# 자료구조 : disjoint-set
+# 자료구조 : disjoint set
 
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
-# find 연산 정의(경로 압축)
-def find(parent, x) :
+# find 연산 (경로압축)
+def find_parent(parent, x) :
     if parent[x] != x :
-        parent[x] = find(parent, parent[x])
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-# union 연산 정의
-def union(parent, a, b) :
-    a = find(parent, a)
-    b = find(parent, b)
+# union 연산
+def union_parent(parent, a, b) :
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
     
     if a < b :
         parent[b] = a
     else :
         parent[a] = b
-        
+
 n = int(input())
 m = int(input())
 
-# 부모 테이블 초기화
+# 부모 테이블 정의
 parent = [v for v in range(n + 1)]
 
 for i in range(1, n + 1) :
-    row = list(map(int, input().split()))
+    connects = list(map(int, input().split()))
     
     for j in range(1, n + 1) :
-        if row[j-1] == 1 :
-            union(parent, i, j)
+        # 도시가 연결되어 있다면
+        if connects[j-1] == 1 :
+            union_parent(parent, i, j)
 
-# 여행 계획 입력
-plans = list(map(int, input().split()))
+# 여행 계획
+plans = list(map(int, input().split()))            
 
-result = [find(parent, plan) for plan in plans]
-if len(set(result)) <= 1 :
+result = set([find_parent(parent, plan) for plan in plans])
+
+if len(result) <= 1 :
     print("YES")
 else :
     print("NO")
