8a7e0b590baa82cd7627ec3f1fc2200d16505397
GinToFim <shlf6673@gmail.com>
2023-10-08 18:20:44 +0900

    [unrated] Title: 옹알이 (2), Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,20 +1,29 @@
+# 아이디어: 
+# 알고리즘: 구현, 문자열
+
 def solution(babbling):
     answer = 0
     
-    # 발음할 수 있는 리스트
-    speak = ['aya', 'ye', 'woo', 'ma']
+    yes_bab = ['aya', 'ye', 'woo', 'ma']
+    no_bab = [y*2 for y in yes_bab]
+
     
     for word in babbling:
-        for s in speak:
-            # 두 번 연속 발음할 수 있는 단어가 없다면
-            if s * 2 not in word:
-                # 발음할 수 있는 단어가 있다면 word를 공백으로 대체
-                # 없다면 아무 일도 일어나지 않음
-                word = word.replace(s, ' ')
-        
-        # 공백을 제거한 문자의 길이가 0이라면
-        if len(word.strip()) == 0:
-            answer += 1
+        # 단어가 없어질 때까지
+        while word:
+            # 앞 두 글자가 가능하면서 앞 네 글자가 연속이지 않다면
+            if word[:2] in yes_bab and word[:4] not in no_bab:
+                # 두 글자 줄이기
+                word = word[2:]
+            # 앞 두 글자가 가능하면서 앞 네 글자가 연속이지 않다면    
+            elif word[:3] in yes_bab and word[:6] not in no_bab:
+                # 세 글자 줄이기
+                word = word[3:]
+            else:
+                break
     
+        # 글자가 남아있지 않다면
+        if not word:
+            answer += 1
     
     return answer
