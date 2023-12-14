9ae0aa4a771173d59be991a2f81802b5d1093c78
GinToFim <shlf6673@gmail.com>
2023-12-14 16:23:35 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,31 +1,32 @@
-# 아이디어: 1. 최대 압축은 (길이 // 2)만큼 가능
-#          2. 완전 탐색으로 1 ~ (길이 // 2)만큼 압축 전부 확인
-#          3. 이전 문자열과 같으면 count 1 추가
-# 알고리즘: 구현, 문자열 (s의 길이가 1,000이기 때문에 완전탐색 가능)
+# 아이디어: 1. 최대 압축은 len(s) // 2 
+#          2. 1 ~ len(s)//2 + 1 에 대한 압축을 완전탐색
+#          3. 압축 횟수(count)가 2 이상이라면 str(count) + prev
+#                                보다 작다면 prev
+# 알고리즘: 문자열, 구현, 완전탐색
 
 def solution(s):
-    answer = len(s) # 최대 길이로 초기화
+    answer = len(s)
     
+    # 압축에 대한 단위(step)
     for step in range(1, len(s) // 2 + 1):
-        compressed = ""
-        prev = s[:step] # 첫 문자열 초기화(for. 이전 문자열 기억)
-        count = 1 # 압축 횟수
+        compressed = "" # 압축을 저장할 문자열
+        prev = s[:step]  # 이전 단어
+        count = 1 # 압축 횟수 
         
-        # step 만큼 증가시키며 이전 단어와 비교
         for j in range(step, len(s), step):
-            # 이전 단어와 동일하다며 압축 횟수 증가
+            # 이전 문자와 같다면
             if prev == s[j: j + step]:
+                # 압축 늘리기
                 count += 1
-            # 다른 문자열이 나왔다면
-            else:
+            else: # 같지 않다면
                 compressed += str(count) + prev if count >= 2 else prev
-                prev = s[j: j + step] # 이전 단어 갱신
-                count = 1 
-        
-        # 마지막 남아 있는 문자열 처리
+                prev = s[j: j + step] # 현재 단어 갱신
+                count = 1
+                
+        # 마지막 단어 처리하기
         compressed += str(count) + prev if count >= 2 else prev
         
-        # 만들어지는 압축 문자열 중 최소 길이 
+        # 압축 단어에 대한 최소 길이 갱신
         answer = min(answer, len(compressed))
         
     return answer
