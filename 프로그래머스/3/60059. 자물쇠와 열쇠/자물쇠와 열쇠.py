a13f02dd31080cda10abc52dcd38117923cb1efa
GinToFim <shlf6673@gmail.com>
2023-12-15 16:30:46 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 1.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,63 +1,61 @@
-# 아이디어: 1. key를 시계방향으로 90도 회전하는 함수 정의
-#          2. lock의 크기를 (N X N) -> (3N X 3N)으로 늘리기 
-#               (정중앙에 원래 lock 정의하기)
-#          3. 자물쇠 중간이 모두 1인지 확인(딱 맞게 자물쇠를 채우기 (0과 2 X))
-# 알고리즘: 구현, 완전 탐색
+# 아이디어: 1. 시계방향 90도 회전 함수 정의(rotate_90)
+#          2. key를 움직이기 -> lock의 범위를 늘리기(N x N -> 3N x 3N)
+#          3. new_lock  정중앙 부분에 lock 집어넣기
+# 알고리즘: 구현
 
 def solution(key, lock):
     n = len(lock)
     m = len(key)
     
-    new_lock = [[0] * (3 * n) for _ in range(3 * n)]
+    # n x n -> 3n x 3n 늘리기
+    new_lock = [[0 for _ in range(3 * n)] for _ in range(3 * n)]
     
-    # 정중앙에 원래 lock 정의하기
-    for i in range(n, 2 * n):
-        for j in range(n, 2 * n):
-            new_lock[i][j] = lock[i-n][j-n]
-    
-    # new_lock 인덱스 -> x, y
-    # key 인덱스 -> i, j
-    
-    # 4가지 방향에 대해서 확인
-    for _ in range(4):
-        key = rotation_90(key, m)
+    # 정중앙 부분에 lock 집어넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
         
-        for x in range(n * 2):
-            for y in range(n * 2):
-                # 자물쇠에 열쇠 끼워 넣기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-                
-                # 새로운 자물쇠에 열쇠가 정확히 맞는지 확인
-                if check(new_lock) == True:
+    # new_lock -> i, j / key -> x, y    
+    # key 4번 회전
+    for _ in range(4):
+        key = rotate_90(key, m)
+        
+        for i in range(2 * n):
+            for j in range(2 * n):
+                for x in range(m):
+                    for y in range(m):
+                        # 자물쇠에 열쇠 채워넣기
+                        new_lock[i + x][j + y] += key[x][y]
+                        
+                # 열쇠가 딱 들어맞았다면
+                if check(new_lock, n):
                     return True
                 
-                # 자물쇠가 맞지 않다면 열쇠 다시 빼기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
+                for x in range(m):
+                    for y in range(m):
+                        # 아니라면 자물쇠에 열쇠를 빼기
+                        new_lock[i + x][j + y] -= key[x][y]
     
     return False
 
-# key(자물쇠) 시계 방향으로 90도 회전
-def rotation_90(key, m):
-    new_key = [[0 for _ in range(m)] for _ in range(m)]
+# 90도 회전 시키는 함수
+def rotate_90(key, m):
+    new_key = [[0] * m for _ in range(m)]
     
     for i in range(m):
         for j in range(m):
             new_key[j][m-1 - i] = key[i][j]
             
     return new_key
-    
-# 자물쇠의 중간 부분이 모두 1인지 확인
-def check(new_lock):
-    lock_length = len(new_lock) // 3
-    
-    for i in range(lock_length, 2 * lock_length):
-        for j in range(lock_length, 2 * lock_length):
-            # 딱 맞지 않다면(하나라도 1이 아니라면)
+
+# 현재 자물쇠와 열쇠가 딱 들어맞았는지 확인하기
+def check(new_lock, n):
+    # 중앙 부분 확인하기
+    for i in range(n, 2 * n):
+        for j in range(n, 2 * n):
+            # 1이 아니라면
             if new_lock[i][j] != 1:
                 return False
-            
+    
     return True
+    
