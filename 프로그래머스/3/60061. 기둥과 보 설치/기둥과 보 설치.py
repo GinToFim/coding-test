45fb51667a806c766eb52f9d199772999afe9232
GinToFim <shlf6673@gmail.com>
2023-12-21 16:45:33 +0900

    [level 3] Title: 기둥과 보 설치, Time: 1552.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,51 +1,54 @@
-# 아이디어: 1. 설치할 때는 일단 설치하고 아니면 삭제(삭제할 때도 마찬가지)
-#          2. 기둥(a == 0)일 때 다음과 같으면 가능
-#               a. 바닥(y == 0)
-#               b. 보의 한 쪽 끝 부분 위에 ([x-1, y, 1] or [x, y, 1])
-#               c. 다른 기둥 위에 ([x, y-1, 0])
-#          3. 보(a == 1)일 때 다음과 같으면 가능
-#               a. 한쪽 끝 부분이 기둥 위에 ([x, y-1, 0] or [x+1, y-1, 0])
-#               b. 양쪽 끝 부분이 동시에 ([x-1, y, 1] and [x+1, y, 1])
+# 아이디어: 1. 기둥(a == 0)
+#               i) 바닥 위(y == 0)
+#               ii) 보의 한쪽 끝 부분 위에( (x, y, 1), (x-1, y, 1))
+#               iii) 또 다른 기둥 위에 (x, y-1, 0)
+#          2. 보(a == 1)
+#               i) 한쪽 끝 부분이 기둥 위에 ( (x, y-1, 0), (x + 1, y-1, 0) )
+#               ii) 양쪽 끝 부분이 동시에 다른 보와 연결 ( (x-1, y, 1) and (x+1, y, 1) )
 # 알고리즘: 구현
-
-def is_possible(answer):
-    for x, y, a in answer:
-        # 기둥일 때
-        if a == 0:
-            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
-                continue
-            
-            # 아니라면
-            return False
-        # 보일 때
-        else:
-            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer
-                                                                    and [x+1, y, 1] in answer):
-                continue
-            
-            # 아니라면
-            return False
-    
-    # 모두 설치가 가능하다면
-    return True
-        
-        
+# 자료구조: set (중복제거)
 
 def solution(n, build_frame):
     answer = []
     
     for x, y, a, b in build_frame:
-        # 삭제(0)라면 일단 삭제하고 불가능하면 다시 삽입
-        if b == 0:
-            answer.remove([x, y, a])
+        # 설치라면(b = 1)
+        if b == 1:
+            # 우선 삽입하고
+            answer.append((x, y, a))
+            # 불가능하다면 다시 삭제
             if not is_possible(answer):
-                answer.append([x, y, a])
-        # 설치(1)라면 일단 삽입하고 불가능하면 다시 삭제
+                answer.remove((x, y, a))
+        # 삭제라면
         else:
-            answer.append([x, y, a])
+            # 우선 삭제하고
+            answer.remove((x, y, a))
+            # 불가능하다면 다시 삽입
             if not is_possible(answer):
-                answer.remove([x, y, a])
-    
+                answer.append((x, y, a))
+        
     # 오름차순 정렬
     answer.sort()
+
     return answer
+
+# 설치가 가능한지 확인
+def is_possible(answer):
+    for x, y, a in answer:
+        if a == 0:  # 기둥(0)이라면
+            # 해당 조건에서 설치가 가능하다면 넘김
+            if y == 0 or (x, y, 1) in answer or (x-1, y, 1) in answer or (x, y-1, 0) in answer:
+                continue
+            
+            # 아니라면 false
+            return False
+        else:   # 보(1)라면
+            # 해당 조건에서 설치가 가능하다면 넘김
+            if (x, y-1, 0) in answer or (x+1, y-1, 0) in answer or ((x-1, y, 1) in answer 
+                                                                    and (x+1, y, 1) in answer) :
+                continue
+            
+            # 아니라면 false
+            return False
+    
+    return True
