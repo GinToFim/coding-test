1cadda9524873ef645fa75fc61447a40bbb048f6
GinToFim <shlf6673@gmail.com>
2023-12-21 17:35:34 +0900

    [level 2] Title: 괄호 변환, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -1,76 +1,85 @@
-# 아이디어:
-# 알고리즘: 
+# 알고리즘: 구현
 
 def solution(p):
-    answer = ''
     # 1. 입력이 빈 문자열인 경우, 빈 문자열 반환
-    if len(p) == 0 :
+    if len(p) == 0:
         return ""
     
-    # 2. 문자열 p를 두 '균형잡힌 괄호 문자열' u, v로 나누기
+    # 2. 두 균형잡힌 괄호 문자열 u, v로 분리
     idx = balanced_index(p)
-    u = p[:idx + 1]
-    v = p[idx + 1:]
     
-    # 3. '올바른 괄호 문자열'이라면 v 붙이기
+    u = p[:idx+1]
+    v = p[idx+1:]
+    
+    # 3. u가 "올바른 괄호 문자열"이라면
     if is_right(u):
-        answer = u + solution(v)
-    # 4. 아니라면
+        return u + solution(v) # v에 대해 재귀적으로 실행
+    # 4. u가 "올바른 괄호 문자열" 아니라면
     else:
-        answer = '(' # 4-1. 빈 문자열에 '(' 붙이기
-        answer += solution(v) # 4-2. v에 대해 재귀적으로 실행
-        answer += ')' # 4-2. ')'를 다시 붙이기
+        # 4-1. 빈 문자열에 '('를 붙이기
+        answer = "("
         
-        # 4-4. 첫 번째와 마지막 문자 제거
+        # 4-2. 문자열 v에 대해 재귀적으로 실행
+        answer += solution(v)
+        
+        # 4-3. ')'를 다시 붙이기
+        answer += ')'
+        
+        # 4-4. u의 첫 번째와 마지막 문자를 삭제하고, 나머지 문자열의 괄호 방향 뒤집기 
         u = list(u[1:-1])
         
-        # 나머지 문자열의 괄호 방향 뒤집어 붙이기
         for i in range(len(u)):
             if u[i] == '(':
                 u[i] = ')'
             else:
                 u[i] = '('
+                
+        # 4-5. 생성된 문자열을 반환
+        return answer + ''.join(u)
         
-        answer += "".join(u)
         
+    
+    
+    
+    
     return answer
 
-# "균형잡힌 괄호 문자열"의 인덱스 반환
-def balanced_index(p):
-    open_count, close_count = 0, 0
+# '균형잡힌 괄호 문자열' 체크 함수
+def balanced_index(w):
+    # 열린 괄호, 닫힌 괄호 개수
+    open_cnt, close_cnt = 0, 0
     
-    for i in range(len(p)):
+    for i in range(len(w)):
         # 열린 괄호라면
-        if p[i] == '(':
-            open_count += 1
+        if w[i] == '(':
+            open_cnt += 1
         # 닫힌 괄호라면
         else:
-            close_count += 1
-    
-        # 열린 괄호와 닫힌 괄호가 같다면 현재 인덱스 반환
-        if open_count == close_count:
+            close_cnt += 1
+        
+        # 열린 괄호와 닫힌 괄호의 개수가 같다면
+        if open_cnt == close_cnt:
             return i
         
-# 해당 괄호가 올바른지 확인
+# "올바른 괄호 문자열" 구하는 함수
 def is_right(u):
-    count = 0 # 열린 괄호 개수
+    open_cnt = 0 # 열린 괄호 개수
     
     for ch in u:
         # 열린 괄호라면
         if ch == '(':
-            count += 1
+            open_cnt += 1
+        # 닫힌 괄호라면
         else:
-            # 쌍이 맞지 않는 경우 false
-            if count == 0:
+            # 열린 괄호가 이미 없다면
+            if open_cnt == 0:
                 return False
             
-            count -= 1
+            open_cnt -= 1
     
-    # 모두 탐색 후에도 스택에 자료가 남았다면
-    if count != 0:
-        return False
-    
+    # "올바른 괄호"라면
     return True
         
         
-    
+        
+        
