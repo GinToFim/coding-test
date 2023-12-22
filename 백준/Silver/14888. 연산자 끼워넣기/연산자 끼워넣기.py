87e39b2fe6982ea87314c06e754ba93508fce2bf
GinToFim <shlf6673@gmail.com>
2023-12-22 15:27:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,5 +1,5 @@
-# 아이디어
-# 알고리즘 : dfs, back tracking
+# 아이디어: 재귀적으로 풀기
+# 알고리즘: dfs
 
 import sys
 input = sys.stdin.readline
@@ -8,30 +8,32 @@ n = int(input())
 data = list(map(int, input().split()))
 add, sub, mul, div = map(int, input().split())
 
-max_value = -1e9
-min_value = 1e9
+min_value = int(1e9) + 1
+max_value = -int(1e9) - 1
 
-def dfs(depth, num, add, sub, mul, div):
-    global max_value, min_value
+
+def dfs(depth, value, add, sub, mul, div):
+    global min_value, max_value
     
-    # 종료조건
+    # 종료조건 정의
     if depth == n:
-        max_value = max(max_value, num)
-        min_value = min(min_value, num)
+        min_value = min(min_value, value)
+        max_value = max(max_value, value)
         return
     
-    if add > 0 :
-        dfs(depth + 1, num + data[depth], add - 1, sub, mul, div)
-        
-    if sub > 0 :
-        dfs(depth + 1, num - data[depth], add, sub - 1, mul, div)
-        
-    if mul > 0 :
-        dfs(depth + 1, num * data[depth], add, sub, mul - 1, div)
-        
-    if div > 0 :
-        dfs(depth + 1, int(num / data[depth]), add, sub, mul, div - 1)
+    if add > 0:
+        dfs(depth + 1, value + data[depth], add - 1, sub, mul, div)
+    
+    if sub > 0:
+        dfs(depth + 1, value - data[depth], add, sub - 1, mul, div)
+    
+    if mul > 0:
+        dfs(depth + 1, value * data[depth], add, sub, mul - 1, div)
+    
+    if div > 0:
+        dfs(depth + 1, int(value / data[depth]), add, sub, mul, div-1)
         
 dfs(1, data[0], add, sub, mul, div)
+
 print(max_value)
 print(min_value)
