4b248228e0cbd600cdabd8b63e3aa4b1d4bfa3cf
GinToFim <shlf6673@gmail.com>
2024-03-20 22:51:21 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,18 +1,18 @@
 # 아이디어: [1, 2, 3, 1]을 찾으면 그만큼 없애기
-# 알고리즘: 브루트 포스
-# 자료구조: 스택
+# 알고리즘: 브루트 포스 
 
 def solution(ingredient):
     answer = 0
-    stack = []
-    
-    for i in ingredient:
-        stack.append(i)
-        
-        if stack[-4:] == [1, 2, 3, 1]:
+    idx = 0
+    while True:
+        # 현재길이가 범위이내라면
+        if idx > len(ingredient) - 4:
+            break
+
+        if ingredient[idx:idx+4] == [1, 2, 3, 1]:
             answer += 1
-            
-            for _ in range(4):
-                stack.pop()
-    
+            ingredient = ingredient[:idx] + ingredient[idx+4:]
+            idx -= 2  
+        
+        idx += 1
     return answer
