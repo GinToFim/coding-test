b7e47e57dd02f1e4074fa8b998de585bc2f45bbf
GinToFim <shlf6673@gmail.com>
2024-04-09 18:56:05 +0900

    [Gold IV] Title: 최단경로, Time: 632 ms, Memory: 68392 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,4 +1,7 @@
-# 다익스트라 : heapq, graph, distacne
+# 아이디어: 1. graph, distance, heapq 정의
+#           2. 특정 정점에서 모든 정점 사이의 거리 확인
+# 알고리즘: 다익스트라
+# 자료구조: 힙(heapq)
 
 import heapq
 import sys
@@ -8,38 +11,43 @@ INF = int(1e9)
 n, m = map(int, input().split())
 start = int(input())
 
-graph = [[] for _ in range(n+1)]
-for _ in range(m) :
-    u, v, w = map(int, input().split())
-    graph[u].append((v, w))
+# 인접 그래프 정의
+graph = [[] for _ in range(n + 1)]
 
-distance = [INF] * (n + 1)
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
-def dijkstra(start) :
-    # 힙큐 및 시작노드 정의
+# 다익스트라 알고리즘 정의
+def dijkstra(start):
+    distance = [INF for _ in range(n + 1)]
+
+    # 힙큐 및 시작점 정의
     hq = []
-    heapq.heappush(hq, (0, start))
+    heapq.heappush(hq, (0, start)) # (거리, 노드) 순으로 저장
     distance[start] = 0
-    
-    # 힙큐가 빌 때까지
-    while hq :
+
+    # 힙큐가 빌 때까지 
+    while hq:
         dist, now = heapq.heappop(hq)
-        
-        # 이미 처리된 적이 있다면 무시(distance[now]보다 길다면)
-        if distance[now] < dist :
+        # 이미 처리된 적이 있다면
+        if distance[now] < dist:
             continue
-        
-        for v, d in graph[now] :
+
+        for v, d in graph[now]:
             cost = dist + d
-            # 합친 거리보다 길다면
-            if cost < distance[v] :
+
+            # 갱신하는 길이가 더 짧다면
+            if distance[v] > cost:
                 distance[v] = cost
                 heapq.heappush(hq, (cost, v))
-                
-dijkstra(start)
 
-for i in range(1, n + 1) :
-    if distance[i] == INF :
+    return distance
+
+
+distance = dijkstra(start)
+for d in distance[1:]:
+    if d == INF:
         print("INF")
-    else :
-        print(distance[i])
+    else:
+        print(d)
