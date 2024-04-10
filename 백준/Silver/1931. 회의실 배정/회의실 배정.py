fc1891cd37930c229ca51edd7e21b2a0335deec2
GinToFim <shlf6673@gmail.com>
2024-04-10 17:55:47 +0900

    [Silver I] Title: 회의실 배정, Time: 232 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,5 +1,5 @@
-# 아이디어 : (끝시간, 시작시간) 형태로 정렬
-# 알고리즘 : 그리디
+# 아이디어: 끝나는 시간을 기준으로 정렬하기
+# 알고리즘: 그리디, 정렬, 활동 선택
 
 import sys
 input = sys.stdin.readline
@@ -7,16 +7,16 @@ input = sys.stdin.readline
 n = int(input())
 meetings = [tuple(map(int, input().split())) for _ in range(n)]
 
-# 끝시간 기준으로 정렬
+# 끝나는 시간 기준으로 오름차순 정렬
 meetings.sort(key = lambda x : (x[1], x[0]))
 
-last_end = 0 # 마지막으로 끝나는 회의시간
-result = 0 # 회의 가능 개수
+result = 0
+last_end = 0 # 마지막에 끝나는 시간
 
-for start, end in meetings :
-    # 다음 회의가 가능하다면
-    if start >= last_end :
+for start, end in meetings:
+    # 활동이 가능하다면
+    if start >= last_end:
         result += 1
         last_end = end
-        
+
 print(result)
