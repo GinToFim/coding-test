95fe002b28ca50ae4db1d0381993f3f2779c7d53
GinToFim <shlf6673@gmail.com>
2024-12-13 11:16:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,55 +1,41 @@
-# 아이디어: 1. bfs 알고리즘을 이용해 단지 수 파악
-#           2. 아파트 단지 수 오름차순 정렬
-# 알고리즘: bfs
-# 자료구조: queue(deque)
+# 아이디어:
+# 알고리즘: dfs
+# 자료구조: stack
 
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
 
-# 상하좌우 정의
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+graph = [list(map(int, input().rstrip()))
+            for _ in range(n)]
 
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
-
-def bfs(x, y):
-    # 큐 및 시작노드 정의
-    queue = deque()
-    queue.append((x, y))
-    graph[x][y] = 0
-    cnt = 1
-
-    # 큐가 빌 때까지
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            # 범위 밖이라면 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-
-            # 해당 지점에 아파트가 있다면
-            if graph[nx][ny] == 1:
-                cnt += 1
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-
-    return cnt
+def dfs(x, y):
+    global cnt
+    # 범위 밖이라면 종료
+    if x < 0 or y < 0 or x >= n or y >= n:
+        return
+    
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        cnt += 1
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        
+    
 
 apts = []
 
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            cnt = bfs(i, j)
+            cnt = 0
+            dfs(i, j)
             apts.append(cnt)
 
+# 오름차순 정렬
 apts.sort()
 
 print(len(apts))
