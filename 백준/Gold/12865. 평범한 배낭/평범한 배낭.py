de460b101292db78b0bb9292f7eaeac8e1532717
GinToFim <shlf6673@gmail.com>
2025-01-12 19:22:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 5248 ms, Memory: 283624 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,16 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
 
-bag = [[0, 0]]
-
-for _ in range(n):
-    bag.append(list(map(int, input().split())))
+bag = [list(map(int, input().split()))
+       for _ in range(n)]
 
 dp = [[0] * (k + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, k + 1):
-        weight = bag[i][0]
-        value = bag[i][1]
+        weight = bag[i-1][0]
+        value = bag[i-1][1]
 
         # 담을 수 있는 무게가 더 작다면
         if j < weight:
