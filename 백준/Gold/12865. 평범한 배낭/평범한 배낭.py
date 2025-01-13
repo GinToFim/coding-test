161c41804bed269653f3e0ac5001be897f5e0cf7
GinToFim <shlf6673@gmail.com>
2025-01-13 23:43:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 5044 ms, Memory: 228328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,22 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
+n, w = map(int, input().split())
+bag = [(0, 0)]
 
-bag = [list(map(int, input().split()))
-       for _ in range(n)]
+for i in range(n):
+    bag.append(tuple(map(int, input().split())))
 
-dp = [[0] * (k + 1) for _ in range(n + 1)]
+dp = [[0] * (w + 1) for _ in range(n + 1)]
 
 for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        weight = bag[i-1][0]
-        value = bag[i-1][1]
+    for j in range(1, w + 1):
+        weight = bag[i][0]
+        value = bag[i][1]
 
-        # 담을 수 있는 무게가 더 작다면
-        if j < weight:
+        if j < weight: # 담을 수 없다면
             dp[i][j] = dp[i-1][j]
-        else: # 현재 보석을 담을 수 있다면
-            dp[i][j] = max(value + dp[i-1][j - weight], dp[i-1][j])
+        else:
+            dp[i][j] = max(dp[i-1][j], value + dp[i-1][j-weight])
 
-print(dp[n][k])
+print(dp[n][w])
