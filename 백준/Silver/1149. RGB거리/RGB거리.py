ff0221bb7249a17562209f3c08734d75284daff2
GinToFim <shlf6673@gmail.com>
2025-01-19 17:31:37 +0900

    [Silver I] Title: RGB거리, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,17 +2,19 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-RGB = []
 
-r, g, b = map(int, input().split())
-RGB = [r, g, b]
+data = [list(map(int, input().split()))
+        for _ in range(n)]
 
-sub_r, sub_g, sub_b = RGB
-for i in range(1, n) :
-	r, g, b = map(int, input().split())
-	sub_r, sub_g, sub_b = RGB
-	RGB[0] = r + min(sub_g, sub_b)
-	RGB[1]  = g + min(sub_r, sub_b)
-	RGB[2]  = b + min(sub_r, sub_g)
-	
-print(min(RGB))
+# dp 테이블 선언
+dp = [[0] * 3 for _ in range(n)]
+
+# 첫 번째는 그대로 복사
+dp[0] = data[0]
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + data[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + data[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + data[i][2]
+
+print(min(dp[n-1]))
