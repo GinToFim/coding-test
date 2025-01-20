115f52c60390a8a3161d58079f1cbe2bacd96a04
GinToFim <shlf6673@gmail.com>
2025-01-20 23:09:30 +0900

    [Gold IV] Title: 스도쿠, Time: 1004 ms, Memory: 114484 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -2,28 +2,18 @@ import sys
 input = sys.stdin.readline
 
 def row_check(r, num):
-    for c in range(9):
-        if board[r][c] == num:
-            return False
-    
+    if row_dict[r][num] != 0:
+        return False
     return True
 
 def col_check(c, num):
-    for r in range(9):
-        if board[r][c] == num:
-            return False
-
+    if col_dict[c][num] != 0:
+        return False
     return True
 
 def square_check(r, c, num):
-    nr = (r // 3) * 3
-    nc = (c // 3) * 3
-
-    for i in range(3):
-        for j in range(3):
-            if board[nr + i][nc + j] == num:
-                return False
-
+    if square_dict[r//3][c//3][num] != 0:
+        return False
     return True
 
 def dfs(depth):
@@ -39,9 +29,14 @@ def dfs(depth):
     for num in range(1, 10):
         if row_check(nr, num) and col_check(nc, num) and square_check(nr, nc, num):
             board[nr][nc] = num
+            row_dict[nr][num] = 1
+            col_dict[nc][num] = 1
+            square_dict[nr//3][nc//3][num] = 1
             dfs(depth + 1)
+            square_dict[nr//3][nc//3][num] = 0
+            col_dict[nc][num] = 0
+            row_dict[nr][num] = 0
             board[nr][nc] = 0
-
 
 board = [list(map(int, input().split()))
         for _ in range(9)]
@@ -49,5 +44,20 @@ board = [list(map(int, input().split()))
 # 0 인 위치 찾기
 zeros = [(i, j) for i in range(9)
         for j in range(9) if board[i][j] == 0]
+
+
+row_dict = [{x : 0 for x in range(1, 10)}
+            for _ in range(9)]
+col_dict = [{x : 0 for x in range(1, 10)}
+            for _ in range(9)]
+square_dict = [[{x : 0 for x in range(1, 10)} for _ in range(3)]
+               for _ in range(3)]
+
+for r in range(9):
+    for c in range(9):
+        if board[r][c] != 0:
+            row_dict[r][board[r][c]] = 1
+            col_dict[c][board[r][c]] = 1
+            square_dict[r//3][c//3][board[r][c]] = 1
 
 dfs(0)
